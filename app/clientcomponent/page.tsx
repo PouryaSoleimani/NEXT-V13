98b1a706de19c02e84a2712e1eb3456fcabd0bd5
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-05-08 16:46:47 -0700

    Client Components

--- app/clientcomponent/page.tsx
+++ app/clientcomponent/page.tsx
@@ -1,15 +1,19 @@
+// ^ CLIENT AND SERVER COMPONENTS
+"use client" // THIS LINE OF CODE MAKES OUR COMPONENT A CLIENT COMPONENT , AND WITHOUT THIS OUR COMPONENT IS ALWAYS A SERVER COMPONENT
 import { NextPage } from 'next'
+import { useState } from 'react'
 
 interface Props { }
 
 const Page: NextPage<Props> = ({ }) => {
 
-  console.log(`CLIENT COMPONENT *****************************`)
+  const [first, setfirst] = useState('HELLO')
 
 
   return (
     <>
       <h1 className='bg-gradient-to-r from-sky-800 via-blue-800 text-4xl font-bold text-center p-6 to-slate-900'>CLIENT COMPONENT</h1>
+      <h2>{first}</h2>
     </>
   )
 }
