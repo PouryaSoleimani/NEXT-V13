8d6c1bb38f025f16c3c08b1bb06b9ac712b213d4
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-18 22:07:12 +0330

    NEXT____V13

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <div>
-      <h2 className="bg-gradient-to-r from-yellow-400 via-purple-500 to-red-800 p-7 text-center font-extrabold text-6xl text-black border-b-8 border-black">HEADER SECTION</h2>
+      <h2 className="bg-gradient-to-r from-yellow-400 via-purple-500 to-red-800 p-7 text-center font-extrabold text-6xl text-black border-b-8 border-black">
+        HEADER SECTION
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
