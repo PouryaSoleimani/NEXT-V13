c282e838e538adafb61a8e6e3be20592dff3198f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-18 23:13:14 +0330

    NEXT____V13

--- app/components/ImageComponent.tsx
+++ app/components/ImageComponent.tsx
@@ -1,18 +1,17 @@
 //^ IMAGE COMPONENT
 
-import Image from "next/image";
-
+import Image from 'next/image';
 
 export default function ImageComponent() {
   return (
     <>
-      <div className="p-8 text-3xl text-center font-bold border-b-8 border-lime-500">
+      <div className="p-8 text-3xl text-center border-b-8 border-lime-500 bg-lime-500 text-black font-black">
         <h1>IMAGE TAG IN NEXT JS</h1>
       </div>
 
       <div className="flex items-center justify-center h-fit py-10">
-        <Image src='/images/cartoonNature.avif' width={1500} height={1000} className="w-[50rem] h-[20rem]" alt="Cartoon Nature Image" />
+        <Image src="/images/cartoonNature.avif" width={1500} height={1000} className="w-[50rem] h-[20rem] rounded-xl" alt="Cartoon Nature Image" />
       </div>
     </>
   );
-}  
+}
--- app/image/page.tsx
+++ app/image/page.tsx
@@ -1,15 +1,15 @@
-import ImageComponent from "../components/ImageComponent";
+import ImageComponent from '../components/ImageComponent';
 
 //^ IMAGE ROUTE
 export default function Page() {
   return (
     <>
-      <div className="text-4xl p-8 text-center font-bold border-b-8 ">
+      <div className="text-4xl p-8 text-center font-bold border-b-8 bg-white text-black">
         <h1>IMAGE ROUTE</h1>
       </div>
 
       <div>
-      <ImageComponent  />
+        <ImageComponent />
       </div>
     </>
   );
