0cad74f4867c27b374c99315a2594f9dfc30dcc2
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-18 22:41:42 +0330

    NEXT____V13

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -18,23 +18,23 @@ const DynamicProductPage = async (params: dynamicProductPageParams) => {
 
   //Return
   return (
-    <>
-      <div className="p-6 text-3xl font-bold text-center border-b-8 border-white">
+    <section className="w-1/2 mx-auto mt-32 rounded-3xl p-8 bg-zinc-900 shadow-2xl">
+      <div className="p-6 text-3xl text-center border-b-8 rounded-t-xl border-white bg-white text-black font-black">
         <h1>DYNAMIC PRODUCT PAGE</h1>
       </div>
 
-      <div className="p-6 text-3xl font-bold text-center border-b-8 border-red-800">
+      <div className="p-6 text-3xl font-bold text-center border-b-8 border-red-800 bg-red-800 text-white">
         <h1>Page Params : {pageParams}</h1>
       </div>
 
       {response && (
-        <div className="p-6 text-3xl font-bold text-center border-b-8 border-blue-800">
+        <div className="p-6 text-3xl font-bold text-center border-b-8 border-blue-800 bg-blue-800 text-white rounded-b-xl">
           <h1>
-            {response.id} -- {response.title} -- {response.price}
+            {response.id} - {response.title.slice(0, 30)} : ${response.price}
           </h1>
         </div>
       )}
-    </>
+    </section>
   );
 };
 
