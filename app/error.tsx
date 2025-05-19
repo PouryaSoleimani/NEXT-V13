e8e814c1bdb677a442437aa30eebde534e845ff1
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-19 21:30:31 +0330

    NEXT____V13

--- app/error.tsx
+++ app/error.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 
 const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
+  console.info(error);
   return (
     <>
       <div className="text-red-600 font-black text-3xl bg-red-200 mx-auto my-16 w-fit p-8 rounded-xl border-4 border-red-500">ERROR MESSAGE : {error.message}</div>
