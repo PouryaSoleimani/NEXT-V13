aa7928275c374919e51aa40117a221768a81f794
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-19 18:02:31 +0330

    NEXT____V13

--- app/loading.tsx
+++ app/loading.tsx
@@ -6,7 +6,7 @@ const Loading = () => {
     <section className="w-screen h-screen flex items-center justify-center">
       <div className="bg-emerald-500 p-6 font-bold text-4xl mx-auto my-32">Loading , Please Wait</div>;
     </section>
-  );
+  );    
 };
 
 export default Loading;
