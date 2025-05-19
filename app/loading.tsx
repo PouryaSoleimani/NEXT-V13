c1939a8912140f1c9928a76326a5b618d57a72e3
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-19 17:11:05 +0330

    NEXT____V13

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,12 @@
 // LOADING COMPONENT
-import React from 'react'
+import React from 'react';
 
 const loading = () => {
   return (
-    <div className='bg-emerald-500 p-6 font-bold text-4xl mx-auto my-32'>loading , Please Wait</div>
-  )
-}
+    <section className="w-screen h-screen flex items-center justify-center">
+      <div className="bg-emerald-500 p-6 font-bold text-4xl mx-auto my-32">loading , Please Wait</div>;
+    </section>
+  );
+};
 
-export default loading
+export default loading;
