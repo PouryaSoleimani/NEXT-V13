6eb26bb3c292b49f9a15463deea3f763fda26b5b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-20 01:35:12 +0330

    NEXT____V13

--- components/AxiosProducts.tsx
+++ components/AxiosProducts.tsx
@@ -3,14 +3,16 @@ import React from 'react';
 interface Props {
   data: { id: number; title: string }[];
 }
+
 const AxiosProducts = (data: Props) => {
-  console.info('CLIENT', data);
+  console.info('CLIENT', data.data);
+
   return (
     <div>
       AxiosProducts
       {data.data.length > 0 ? (
         data.data.map((item) => (
-          <h2 key={item.id} className="bg-pink-500 py-4 my-4 px-4 rounded w-fit text-black">
+          <h2 key={item.id} className="bg-pink-500 py-4 my-4 px-4 rounded w-fit text-black font-black text-2xl ">
             {item.title}
           </h2>
         ))
