a3b8ba7530947a915465de72d6ae8ce20ddd9aa8
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-20 15:23:03 +0330

    NEXT____V13

--- Axios/AxiosInstance.js
+++ Axios/AxiosInstance.js
@@ -5,5 +5,15 @@ const AXIOS = axios.create({
   headers: { 'Content-Type': 'application/json' },
   timeout: 10000,
 });
-
+// INTERCEPTORS
+AXIOS.interceptors.request.use(
+  (config) => {
+    console.log('CONFIG ===>', config);
+    return config;
+  },
+  (error) => {
+    console.log('ERROR  ===>', error);
+    return Promise.reject(error);
+  },
+);
 export default AXIOS;
