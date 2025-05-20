1d17cd9606b8243d340f1929078b86ef5088efb7
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-20 21:26:34 +0330

    NEXT____V13

--- app/error.tsx
+++ app/error.tsx
@@ -8,7 +8,9 @@ export default function ErrorBoundary({ error, reset }: { error: Error & { diges
     <div className="bg-zinc-800 p-10 rounded-xl mx-auto my-10 w-fit space-y-8">
       <h2>Something went wrong!</h2>
       <p className="bg-black/50 text-2xl font-black tracking-tight text-red-800">{error.message}</p>
-      <button onClick={() => reset()} className="bg-indigo-700 px-4 py-2 font-black rounded-xl">Try again</button>
+      <button onClick={() => reset()} className="bg-indigo-700 px-4 py-2 font-black rounded-xl">
+        Try again
+      </button>
     </div>
   );
 }
