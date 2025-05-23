0ad08d39d972087a8422ad5c86e3691a391c9ba3
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-24 02:12:25 +0330

    NEXT___V13___ADVANCED

---
+++ components/BackButton.tsx
@@ -0,0 +1,17 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+const BackButton = () => {
+  const router = useRouter();
+  function backhandler() {
+    router.back();
+  }
+  return (
+    <button onClick={backhandler} className="bg-sky-700 px-4 py-2 rounded mx-auto my-10">
+      BACK
+    </button>
+  );
+};
+
+export default BackButton;
