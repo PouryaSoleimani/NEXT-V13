15f704d561cc42d4dded5cc7fda1e225a97fa70b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-24 01:00:02 +0330

    NEXT___V13___ADVANCED

--- app/(routegroup)/adminpanel/page.tsx
+++ app/(routegroup)/adminpanel/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const AdminPanel = () => {
-  return (
-    <div className='bg-pink-500 p-6 font-bold text-4xl text-center'>AdminPanel</div>
-  )
-}
+  return <div className="bg-pink-500 p-6 font-bold text-4xl text-center">AdminPanel</div>;
+};
 
-export default AdminPanel
+export default AdminPanel;
