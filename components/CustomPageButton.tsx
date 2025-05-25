745c5106ac540748786ebf7104af3d5ca2c003cb
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-25 22:12:24 +0330

    NEXT___V13___ADVANCED

--- components/CustomPageButton.tsx
+++ components/CustomPageButton.tsx
@@ -1,13 +1,21 @@
 'use client';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import React from 'react';
 
 const CustomPageButton = () => {
   const router = useRouter();
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
 
-  const handleClick = () => {};
-
-  return <button onClick={handleClick}>CustomPageButton</button>;
+  const handleClick = () => {
+    router.push('/', { scroll: true });
+  };
+  console.info(pathName, searchParams);
+  return (
+    <button onClick={handleClick} className="bg-orange-500 px-6 py-2 rounded-lg text-black font-bold">
+      CustomPageButton
+    </button>
+  );
 };
 
 export default CustomPageButton;
