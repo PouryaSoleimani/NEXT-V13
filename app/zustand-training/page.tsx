02596208da2876188c8cef020ed98f98fc548b3f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-27 19:22:37 +0330

    NEXT___V13___ADVANCED

---
+++ app/zustand-training/page.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const ZustandTraining = () => {
+    return (
+        <div className='h-[90vh]'>
+            <h2 className='bg-emerald-700 text-center py-5 text-3xl font-black'>Zustand Training</h2>
+        </div>
+    )
+}
+
+export default ZustandTraining
