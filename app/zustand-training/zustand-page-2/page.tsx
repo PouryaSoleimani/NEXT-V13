36d1c6aa7215424296bbab214642d76c0e4f5724
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-05-27 20:07:23 +0330

    ZUSTAND

---
+++ app/zustand-training/zustand-page-2/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import useCounterStore from '@/zustand/store'
+import React from 'react'
+
+const ZustandPage2 = () => {
+    const { count } = useCounterStore() as any
+    return (
+        <div className='text-6xl text-center font-bold'>
+            {count}
+        </div>
+    )
+}
+
+export default ZustandPage2
