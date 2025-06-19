be48529887c4e6552758bc009473148d2992d245
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-19 19:44:57 +0330

    ZUSTAND

--- zustand/store.ts
+++ zustand/store.ts
@@ -1,12 +1,12 @@
-import { create } from "zustand";
-type CounterStoreType = { count: number }
+import { create } from 'zustand';
+type CounterStoreType = { count: number };
 
 const useCounterStore = create((set) => ({
-    count: 0,
-    increase: () => set((state: CounterStoreType) => ({ count: state?.count + 1 })),
-    decrease: () => set((state: CounterStoreType) => ({ count: state?.count - 1 })),
-    changeTo100: () => set({ count: 100 }),
-    reset: () => set({ count: 0 })
-}))
+  count: 0,
+  increase: () => set((state: CounterStoreType) => ({ count: state?.count + 1 })),
+  decrease: () => set((state: CounterStoreType) => ({ count: state?.count - 1 })),
+  changeTo100: () => set({ count: 100 }),
+  reset: () => set({ count: 0 }),
+}));
 
-export default useCounterStore
+export default useCounterStore;
