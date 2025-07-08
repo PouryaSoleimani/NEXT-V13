199e6d054e44c2de4598dab948fce49836b6d0e1
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-07-08 19:24:42 +0330

    ZUSTAND

---
+++ zustand/useFormStore.ts
@@ -0,0 +1,21 @@
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+type useFormStoreType = {
+  datas: [];
+  addDatas: (state: [], datas: {} | []) => void;
+};
+
+const useFormStore = create()(
+  persist(
+    (_set) => ({
+      datas: [],
+      addDatas: (state: any, datas: any) => {
+        datas: [...state.datas, datas];
+      },
+    }),
+    {
+      name: '___FORM_STORE',
+      storage: () => createJSONStorage(localStorage),
+    },
+  ),
+);
