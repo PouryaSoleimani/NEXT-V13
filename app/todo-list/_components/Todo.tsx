f5f8cb47a6fd0ba7ed37b748b69cb84bbc5a1d95
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-07-09 20:03:09 +0330

    🟦 REACT____TS___TODOLIST ✔️

--- app/todo-list/_components/Todo.tsx
+++ app/todo-list/_components/Todo.tsx
@@ -8,11 +8,11 @@ import useTodoStore from '@/app/zustand-training/useTodoStore'
 const Todo = ({ id, text, completed }: TodoType) => {
 
   const { removeTodo } = useTodoStore()
-  
+
   return (
-    <li className={`flex items-center justify-between p-2 border-b border-b-zinc-600 bg-black rounded-lg pl-3 text-xl font-black ${completed == true ? 'line-through opacity-50' : 'text-white'}`}>
+    <li className={`flex items-center justify-between p-2 border-b border-b-zinc-600 bg-black rounded-lg pl-3 my-3 text-xl font-black ${completed == true ? 'line-through opacity-50' : 'text-white'}`}>
       <span className='text-white'>{text}</span>
-      <button className='text-red-500 bg-red-100 p-3 rounded hover:bg-red-200' onClick={() => removeTodo(id)}><BiTrash className='w-6 h-6' /></button>
+      <button className='bg-red-900 text-white p-3 rounded hover:bg-red-800' onClick={() => removeTodo(id)}><BiTrash className='w-6 h-6' /></button>
     </li>
   )
 }
