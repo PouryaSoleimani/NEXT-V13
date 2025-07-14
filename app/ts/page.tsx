de0b047aee9537f64f1e6a499a0f2be3314c774d
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-07-14 21:47:40 +0330

    ⬛ HEADER____SHADCN

--- app/ts/page.tsx
+++ app/ts/page.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const TsTrainingPage = () => {
   return (
     <div>
-      <h1>TypeScript Training</h1>
+      <h1 className='text-4xl font-bold bg-blue-700 py-5 text-center w-fit px-4 rounded-lg mx-auto my-5 border-b-4 border-white'>TypeScript Training</h1>
+      <div>
+
+      </div>
     </div>
   )
 }
