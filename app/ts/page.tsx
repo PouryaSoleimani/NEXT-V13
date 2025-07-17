e528897f661cad8415093b8d01625e5be060ce57
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-07-17 21:13:16 +0330

    🟦 TS___REVIEW

--- app/ts/page.tsx
+++ app/ts/page.tsx
@@ -10,12 +10,7 @@ const TsTrainingPage = () => {
   console.info('RESULT ==> ', _Result)
 
   ReturnProductStatus('PENDING')
-
-
-
-
-
-
+  
   return (
     <div>
       <h1 className='text-4xl font-bold bg-blue-700 py-5 text-center w-fit px-4 rounded-lg mx-auto my-5 border-b-4 border-white'>TypeScript Training</h1>
