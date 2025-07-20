f98e00a657aa0ef29de45ac56570ff2beb73dc9b
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-07-20 23:06:45 +0330

    🟦 TS___REVIEW___TODO___LIST ✅

--- app/zustand-training/useTodoStore.ts
+++ app/zustand-training/useTodoStore.ts
@@ -22,8 +22,11 @@ const useTodoStore = create<TodoState>()(
         set((state) => ({
           todos: state.todos.map((todo) => (todo.id === id ? { ...todo, completed: !todo.completed } : todo)),
         })),
-      removeAllTodos: () => set((state) => ({ todos: [] })),
-      makeRandomTodo: () => set((state) => ({ todos: [...state.todos, { id: crypto.randomUUID(), text: Math.random().toLocaleString(), completed: false }] as any })),
+      removeAllTodos: () => set(() => ({ todos: [] })),
+      makeRandomTodo: () => set((state) => ({
+        todos: [...state.todos,
+        { id: crypto.randomUUID(), text: Math.random().toLocaleString(), completed: false }] as any
+      })),
       toggleAllTodosComplete: () => set((state) => ({
         todos: state.todos.map((todo) => ({ ...todo, completed: !todo.completed })),
       }))
