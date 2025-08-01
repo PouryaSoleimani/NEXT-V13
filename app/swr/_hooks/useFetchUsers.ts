d8005be4304d5bae7312bf2fa955aa2cf8d845d9
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-08-01 23:01:41 +0330

    ⬛  SWR

--- app/swr/_hooks/useFetchUsers.ts
+++ app/swr/_hooks/useFetchUsers.ts
@@ -2,15 +2,21 @@
 import axios from 'axios'
 import React from 'react'
 import useSWR from 'swr'
-
-
-
+export type SingleUserType = {
+  id: string | number,
+  email: string,
+  username: string,
+  password: string,
+  name: {
+    firstname: string,
+    lastname: string
+  },
+  phone: string
+}
 const Usersfetcher = (url: string) => axios.get(url).then(res => res.data)
-
 function useFetchUsers() {
-
   const { data, isLoading, error } = useSWR('https://fakestoreapi.com/users', Usersfetcher)
-  return { data, isLoading, error }
+  return { data: data as SingleUserType[], isLoading, error }
 }
 
 export default useFetchUsers
--- app/swr/page.tsx
+++ app/swr/page.tsx
@@ -3,14 +3,13 @@ import axios from 'axios'
 import { LoaderCircle } from 'lucide-react'
 import React from 'react'
 import useSWR from 'swr'
+import useFetchUsers, { SingleUserType } from './_hooks/useFetchUsers'
 
 const fetcher = (url: string) => axios.get(url).then(res => res.data)
 
 function SwrPage() {
 
-  const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
-
-  console.info('SWR ==>', data)
+  const { data: users, error, isLoading } = useFetchUsers()
 
   if (isLoading) return (
     <div className='w-screen h-screen flex items-center justify-center'>
@@ -21,10 +20,11 @@ function SwrPage() {
   return (
     <div className='bg-black p-4 rounded-md my-6 w-[90%] mx-auto shadow'>
       <h1 className='w-full px-4 text-start bg-orange-600/50 text-2xl font-bold border-b-4 border-white  py-4 rounded-md'>Posts</h1>
-      {data?.slice(0, 5).map((post: any) => (
-        <div key={post.id} className='p-2 font-mono font-semibold text-lg border-b'>
-          <h3>{post.id}. {post.title.slice(0, 20)}</h3>
-          <p>{post.body.slice(0, 20)}</p>
+      {users?.slice(0, 5)?.map((user: SingleUserType) => (
+        <div key={user.id} className='p-2 font-mono font-semibold text-lg border-b'>
+          <h3>{user.id}. {user.name.firstname} {user.name.lastname}</h3>
+          <p>{user.email}</p>
+          <p>{user.phone}</p>
         </div>
       ))}
     </div>
