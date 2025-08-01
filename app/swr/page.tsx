cb0456d7db02d743375e65533d329bc61c64c86c
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-08-01 23:09:16 +0330

    ⬛  SWR

--- app/swr/page.tsx
+++ app/swr/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import axios from 'axios'
-import { LoaderCircle } from 'lucide-react'
+import { LoaderCircle, Lock, Mail, Phone, User } from 'lucide-react'
 import React from 'react'
 import useSWR from 'swr'
 import useFetchUsers, { SingleUserType } from './_hooks/useFetchUsers'
@@ -20,13 +20,16 @@ function SwrPage() {
   return (
     <div className='bg-black p-4 rounded-md my-6 w-[90%] mx-auto shadow'>
       <h1 className='w-full px-4 text-start bg-orange-600/50 text-2xl font-bold border-b-4 border-white  py-4 rounded-md'>Posts</h1>
-      {users?.slice(0, 5)?.map((user: SingleUserType) => (
-        <div key={user.id} className='p-2 font-mono font-semibold text-lg border-b'>
-          <h3>{user.id}. {user.name.firstname} {user.name.lastname}</h3>
-          <p>{user.email}</p>
-          <p>{user.phone}</p>
-        </div>
-      ))}
+      <div className='grid grid-cols-4 p-3'>
+        {users?.slice(0, 5)?.map((user: SingleUserType) => (
+          <div key={user.id} className='p-2 font-mono font-semibold text-lg border-b-4 border-r-4 border-t-4 border-zinc-600 first:border-l-4 last:border-l-4 last:border-t-0 '>
+            <h3 className='flex items-center gap-3 uppercase'><User className='size-5' />{user.name.firstname} {user.name.lastname}</h3>
+            <p className='text-blue-700 flex items-center gap-3'><Mail className='size-5' />{user.email}</p>
+            <p className='text-green-800 flex items-center gap-3'><Phone className='size-5' />{user.phone}</p>
+            <p className='text-red-700/50 hover:text-red-700 flex items-center gap-3'><Lock className='size-5' />{user.password}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
