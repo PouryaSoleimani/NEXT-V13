e239d8e65a73357d98f3f1b4de71cf2fdc8b3b32
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-08-01 21:37:19 +0330

    ⬛ SWR

--- app/swr/page.tsx
+++ app/swr/page.tsx
@@ -1,6 +1,13 @@
+import axios from 'axios'
 import React from 'react'
+import useSWR from 'swr'
+
 
 function SwrPage() {
+  const fetcher = (url: string) => { axios.get(url).then(res => res.data) }
+  const { data: SWR__DATA, error, isLoading } = useSWR('https://fakestoreapi.com/products', fetcher)
+
+  console.info('SWR ==>', SWR__DATA)
   return (
     <div>SwrPage</div>
   )
