cf11920f67fae07fd35951e311385f6e49a04717
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-08-09 12:46:24 +0330

    LODASH___NTH

---
+++ app/lodash/db.js
@@ -0,0 +1,23 @@
+export const array = [10, 20, 50, 60, 30, 70, 90, 100];
+export const array2 = [10, 20, 110, 120];
+
+export const users = [
+  { id: 1, name: 'ali', age: 25 },
+  { id: 2, name: 'reza', age: 22 },
+  { id: 3, name: 'pourya', age: 32 },
+  { id: 4, name: 'mamad', age: 25 },
+  { id: 5, name: 'zahra', age: 21 },
+  { id: 6, name: 'ashkan', age: 21 },
+];
+
+export const products = [
+  { id: 1, title: 'BAG', category: 'accessories', price: 100 },
+  { id: 2, title: 'CAPS', category: 'accessories', price: 50 },
+  { id: 3, title: 'SHIRT', category: 'clothes', price: 200 },
+  { id: 4, title: 'PANTS', category: 'clothes', price: 150 },
+  { id: 5, title: 'JACKET', category: 'clothes', price: 300 },
+  { id: 6, title: 'SNEAKERS', category: 'shoes', price: 250 },
+  { id: 7, title: 'BOOTS', category: 'shoes', price: 350 },
+];
+
+export const falsyIncludedArray = [1, 20, false, '', 0, null, undefined, NaN, 30, 'pourya'];
--- app/lodash/page.tsx
+++ app/lodash/page.tsx
@@ -1,30 +1,7 @@
 import React from 'react';
 import * as _ from 'lodash';
 import { StarIcon } from 'lucide-react';
-
-const array = [10, 20, 50, 60, 30, 70, 90, 100];
-const array2 = [10, 20, 110, 120];
-
-const users = [
-  { id: 1, name: 'ali', age: 25 },
-  { id: 2, name: 'reza', age: 22 },
-  { id: 3, name: 'pourya', age: 32 },
-  { id: 4, name: 'mamad', age: 25 },
-  { id: 5, name: 'zahra', age: 21 },
-  { id: 6, name: 'ashkan', age: 21 },
-];
-
-const products = [
-  { id: 1, title: 'BAG', category: 'accessories', price: 100 },
-  { id: 2, title: 'CAPS', category: 'accessories', price: 50 },
-  { id: 3, title: 'SHIRT', category: 'clothes', price: 200 },
-  { id: 4, title: 'PANTS', category: 'clothes', price: 150 },
-  { id: 5, title: 'JACKET', category: 'clothes', price: 300 },
-  { id: 6, title: 'SNEAKERS', category: 'shoes', price: 250 },
-  { id: 7, title: 'BOOTS', category: 'shoes', price: 350 },
-];
-
-const falsyIncludedArray = [1, 20, false, '', 0, null, undefined, NaN, 30, 'pourya'];
+import { array, users, products, falsyIncludedArray, array2 } from './db';
 
 // COMPONENT ===============================================================================================================================================================================================================
 function LodashPage() {
@@ -99,6 +76,9 @@ function LodashPage() {
 
   const lastItem = _.last(products);
   // console.info('lastItem', lastItem);
+
+  const nth = _.nth(products, 3);
+  console.info('nth', nth);
 
   return (
     <div className="w-screen h-screen flex items-center justify-center">
