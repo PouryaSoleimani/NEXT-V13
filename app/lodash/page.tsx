3fd84b9c83139932f73b9cb8b1097b93b162369f
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-08-09 11:43:09 +0330

    LODASH___GROUPBY

--- app/lodash/page.tsx
+++ app/lodash/page.tsx
@@ -11,6 +11,16 @@ const users = [
   { id: 3, name: 'ashkan', age: 21 },
 ];
 
+const products = [
+  { id: 1, title: 'BAG', category: 'accessories' },
+  { id: 2, title: 'CAPS', category: 'accessories' },
+  { id: 3, title: 'SHIRT', category: 'clothes' },
+  { id: 4, title: 'PANTS', category: 'clothes' },
+  { id: 5, title: 'JACKET', category: 'clothes' },
+  { id: 6, title: 'SNEAKERS', category: 'shoes' },
+  { id: 7, title: 'BOOTS', category: 'shoes' },
+];
+
 // COMPONENT ===============================================================================================================================================================================================================
 function LodashPage() {
   const sortedArray = _.sortBy(array).reverse();
@@ -20,7 +30,10 @@ function LodashPage() {
   // console.info('sortedUsers', sortedUsers);
 
   const groupedUsers = _.groupBy(users, 'age');
-  console.info('groupedUsers', groupedUsers);
+  // console.info('groupedUsers', groupedUsers);
+
+  const groupProducts = _.groupBy(products, 'category');
+  console.info('groupProducts', groupProducts);
 
   const chunkedUsers = _.chunk(users, 2);
   console.info('chunkedUsers', chunkedUsers);
