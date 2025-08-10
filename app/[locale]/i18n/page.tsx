39ac694e8095a8cd4ebac8e46811d83ae81eb986
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-08-10 18:26:20 +0330

    NEXT___INTERNATIONAL

--- app/[locale]/i18n/page.tsx
+++ app/[locale]/i18n/page.tsx
@@ -4,8 +4,8 @@ import { useI18n, useScopedI18n, useChangeLocale } from '@/locales/client'
 
 function NextInternationalPage() {
   const changeLocale = useChangeLocale()
-
   const t = useI18n()
+
   return (
     <div className='w-screen h-screen flex flex-col gap-5 items-center justify-center text-lg font-[Vazir]'>
       <span className='text-4xl bg-zinc-800 font-black  p-5 rounded-lg border-4 border-zinc-300'>
