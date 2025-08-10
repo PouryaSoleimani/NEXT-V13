fc3c2cfca4041ce60c4347a7879a61515a75b291
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-08-10 12:00:31 +0330

    REMEDA

--- app/remeda/page.tsx
+++ app/remeda/page.tsx
@@ -1,37 +1,49 @@
 import React from 'react';
-import * as R from 'remeda';
+import * as _ from 'remeda';
 import { products, users } from '../lodash/db';
+import { indexOf } from 'lodash';
 
 function Remeda() {
-  const sorted = R.sort(products, (item) => item.price).reverse();
+  const sorted = _.sort(products, (item) => item.price).reverse();
   // console.info(sorted);
 
-  const partitioned = R.partition(products, (item) => item.category == 'accessories');
+  const partitioned = _.partition(products, (item) => item.category == 'accessories');
   // console.info(partitioned);
 
-  const chuncked = R.chunk(products, 3);
+  const chuncked = _.chunk(products, 3);
   // console.info(chuncked)
 
-  const concated = R.concat(products, users);
+  const concated = _.concat(products, users);
   // console.info(concated)
 
-  const filtered = R.filter(products, (item) => item.category == 'shoes');
+  const filtered = _.filter(products, (item) => item.category == 'shoes');
   // console.info(filtered)
 
-  const grouped = R.groupBy(products, (item) => item.category);
+  const grouped = _.groupBy(products, (item) => item.category);
   // console.info(grouped);
 
-  const mapped = R.map(products, (item) => item.title);
+  const mapped = _.map(products, (item) => item.title);
   // console.info(mapped);
 
-  const reduced = R.reduce(products, (sum, item) => sum + item.price, 0);
+  const reduced = _.reduce(products, (sum, item) => sum + item.price, 0);
   // console.info(reduced);
 
+  const foundIndex = _.findIndex(products, (item) => item.id == 2);
+  // console.info(foundIndex);
+
+  const first = _.first(products);
+  // console.info(first)
+
+  const randomString = _.randomString(10);
+  console.info(randomString);
 
   return (
     <div>
       <h2 className="text-lime-400 w-fit mx-auto mt-5 bg-zinc-800 px-5 py-2.5 text-2xl font-black font-sans rounded-xl border-4 border-zinc-300">
         REMEDA
+      </h2>
+      <h2 className="w-fit mx-auto mt-10 bg-zinc-800 border-4 border-lime-400 p-5">
+        PASSWORD: <span className="ml-4 bg-black p-2">{randomString}</span>
       </h2>
     </div>
   );
