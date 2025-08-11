17a117d8e96a81fabfc6b9db93f337bf94d3cafd
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-08-11 19:02:13 +0330

    ⬛ NEXT___INTERNATIONAL

--- app/[locale]/_components/Header.tsx
+++ app/[locale]/_components/Header.tsx
@@ -1,22 +1,28 @@
-import { useChangeLocale } from '@/locales/client'
-import Image from 'next/image'
-import React from 'react'
+import { useChangeLocale } from '@/locales/client';
+import Image from 'next/image';
+import React from 'react';
 
 function Header() {
-  const changeLocale = useChangeLocale()
+  const changeLocale = useChangeLocale();
 
   return (
-    <div className='px-5 w-screen'>
-      <div id="BUTTONS" className='mt-4 flex items-center mx-10 justify-between gap-4 bg-black p-5 rounded-lg'>
+    <div className="px-5 w-screen">
+      <div id="BUTTONS" className="mt-4 flex items-center mx-10 justify-between gap-4 bg-black p-5 rounded-lg">
         <div>LOGO</div>
-        <div className='flex gap-3'>
-          <button className='hover:scale-110 duration-300' onClick={() => changeLocale('en')}><Image src={'/images/iran.svg'} width={35} height={30} alt='farsi' /></button>
-          <button className='hover:scale-110 duration-300' onClick={() => changeLocale('fa')}><Image src={'/images/england.svg'} width={35} height={30} alt='english' /></button>
-          <button className='hover:scale-110 duration-300' onClick={() => changeLocale('ar')}><Image src={'/images/saudi-arabia.svg'} width={35} height={30} alt='english' /></button>
+        <div className="flex gap-3">
+          <button className="hover:scale-110 duration-300" onClick={() => changeLocale('en')}>
+            <Image src={'/images/iran.svg'} width={35} height={30} alt="farsi" />
+          </button>
+          <button className="hover:scale-110 duration-300" onClick={() => changeLocale('fa')}>
+            <Image src={'/images/england.svg'} width={35} height={30} alt="english" />
+          </button>
+          <button className="hover:scale-110 duration-300" onClick={() => changeLocale('ar')}>
+            <Image src={'/images/saudi-arabia.svg'} width={35} height={30} alt="english" />
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- app/[locale]/home/page.tsx
+++ app/[locale]/home/page.tsx
@@ -1,25 +1,22 @@
-"use client"
-import React from 'react'
-import { useI18n, useScopedI18n, useChangeLocale } from '@/locales/client'
-import Header from '../_components/Header'
-
-
+'use client';
+import React from 'react';
+import { useI18n, useScopedI18n, useChangeLocale } from '@/locales/client';
+import Header from '../_components/Header';
 
 function NextInternationalPage() {
-
-  const t = useI18n()
+  const t = useI18n();
 
   return (
-    <div className='w-screen h-screen flex flex-col gap-5 items-center justify-between text-lg font-[Vazir]'>
+    <div className="w-screen h-screen flex flex-col gap-5 items-center justify-between text-lg font-[Vazir]">
       <Header />
-      <span className='text-md w-[80%] bg-zinc-800 font-medium text-zinc-300  p-5 rounded-lg border-4 border-zinc-300'>
+      <span className="text-md w-[80%] bg-zinc-800 font-medium text-zinc-300  p-5 rounded-lg border-4 border-zinc-300">
         {t('text')}
       </span>
-      <div className='flex items-center gap-3'>
-        <button className='p-2 rounded-lg border-2 border-zinc-300 w-16 text-center bg-green-600'>EN</button>
+      <div className="flex items-center gap-3">
+        <button className="p-2 rounded-lg border-2 border-zinc-300 w-16 text-center bg-green-600">EN</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default NextInternationalPage
+export default NextInternationalPage;
