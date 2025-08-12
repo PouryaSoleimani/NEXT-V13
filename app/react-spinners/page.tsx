3979f0d7ab7bc59243e63784443c1f1158ac4451
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-08-12 22:31:26 +0330

    � REACT__SPINNERS

--- app/react-spinners/page.tsx
+++ app/react-spinners/page.tsx
@@ -4,9 +4,7 @@ import { FadeLoader } from "react-spinners";
 
 function ReactSpinners() {
   let [loading, setLoading] = useState(true);
-  let [color, setColor] = useState("#e10808");
-
-  setTimeout(() => { setLoading(false) }, 2000);
+  let [color, setColor] = useState("#ffffff");
 
   return (
     <div className="sweet-loading">
@@ -18,11 +16,6 @@ function ReactSpinners() {
       <div className="flex items-center justify-center">
         <FadeLoader color={color} loading={loading} speedMultiplier={0.8} width={5} height={15} className="scale-50" aria-label="Loading Spinner" data-testid="loader" />
       </div>
-      {!loading && (
-        <div className="flex items-center justify-center text-xl font-black bg-emerald-800 w-fit mx-auto px-5 py-2 rounded-xl">
-          <h2>DONE</h2>
-        </div>
-      )}
     </div>
   );
 }
