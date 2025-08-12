d1133f266a939e4920eecbcba3b6ed1d1c2a911b
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-08-12 22:39:54 +0330

    🟥 REACT___SPINNERS

--- app/react-spinners/page.tsx
+++ app/react-spinners/page.tsx
@@ -1,21 +1,16 @@
 "use client"
-import { useState } from "react";
-import { FadeLoader } from "react-spinners";
+import { useState, CSSProperties } from "react";
+import { ClipLoader } from "react-spinners";
+
 
 function ReactSpinners() {
   let [loading, setLoading] = useState(true);
   let [color, setColor] = useState("#ffffff");
 
   return (
-    <div className="sweet-loading">
-      <div className="flex items-center justify-center py-10">
-        <button className="btn mx-auto" onClick={() => setLoading(!loading)} >
-          Toggle Loader
-        </button>
-      </div>
-      <div className="flex items-center justify-center">
-        <FadeLoader color={color} loading={loading} speedMultiplier={0.8} width={5} height={15} className="scale-50" aria-label="Loading Spinner" data-testid="loader" />
-      </div>
+    <div className="sweet-loading flex flex-col items-center">
+      <button onClick={() => setLoading(!loading)} className="btn mx-auto my-10">Toggle Loader</button>
+      <ClipLoader color={color} loading={loading} speedMultiplier={0.8} size={35} className="stroke-3" aria-label="Loading Spinner" data-testid="loader" />
     </div>
   );
 }
