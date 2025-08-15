93d9a2854dbf65871f525b20caf8d2ed94c650ec
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-08-15 22:46:12 +0330

    ⬛ DND-KIT____TODOS

--- app/dnd-kit/page.tsx
+++ app/dnd-kit/page.tsx
@@ -1,18 +1,24 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DndContext } from '@dnd-kit/core';
 import Draggable from './_components/Draggable';
 import Droppable from './_components/Droppable';
+import Logger from '@/hooks/Logger';
 
 function DndKitPage() {
   const containers = ['A'];
-  const todos = ['Go to GYM', 'Learn NEXT', 'Learn REACT'];
+  const [Todos, setTodos] = useState<Array<any>>([
+    { id: 1, title: 'Go to GYM' },
+    { id: 2, title: 'Learn NEXT' },
+    { id: 3, title: 'Learn REACT' },
+  ]);
   const [parent, setParent] = useState<string | null>(null);
-  const [activeTodo, setActiveTodo] = useState<string | null>(null);
+  const [activeTodo, setActiveTodo] = useState<any>(null);
 
-  const draggableMarkup = (text: string) => (
-    <Draggable id={text}>{text}</Draggable>
-  );
+  const draggableMarkup = (text: string) => <Draggable id={text}>{text}</Draggable>;
+  useEffect(() => {
+    Logger("ACTIVE TODO", 'log', activeTodo)
+  }, [activeTodo])
 
   function handleDragEnd(event: any) {
     const { active, over } = event;
@@ -28,18 +34,18 @@ function DndKitPage() {
   return (
     <DndContext onDragEnd={handleDragEnd}>
       <div className="flex gap-3">
-        {parent === null &&
-          todos.map((item, index) => (
-            <div key={index}>{draggableMarkup(item)}</div>
-          ))}
+        {
+          // parent === null &&
+          Todos.map((item, index) => (
+            <div key={index}>{draggableMarkup(item?.title?.toString())}</div>
+          ))
+        }
       </div>
 
       <div className="flex gap-3 w-screen h-screen items-start justify-center p-10 grow">
         {containers.map((id) => (
           <Droppable key={id} id={id} parent={parent}>
-            {parent === id && activeTodo
-              ? draggableMarkup(activeTodo)
-              : 'DONE TODOS'}
+            {parent === id && activeTodo ? draggableMarkup(activeTodo) : 'DONE TODOS'}
           </Droppable>
         ))}
       </div>
