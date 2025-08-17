dd6aebd2b57c930cb280a93e9744ad0bbeb9e3a1
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-08-17 19:59:28 +0330

    🟪 STYLED____COMPONENTS

--- app/styled-components/page.tsx
+++ app/styled-components/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
 const Button: any = styled.button`
   background-color: ${(props: any) =>
@@ -26,26 +26,50 @@ const Button: any = styled.button`
   border: ${(props: any) => (props.variant === 'outline' ? '2px solid #aaa' : 'none')};
 `;
 
-const SecondButton = styled(Button)`
+const SecondButton = styled(Button).attrs({ type: 'submit', as: 'a' })`
   box-shadow: none;
   transition: all 300ms linear;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   &:hover {
     box-shadow: 2px 2px 1px #2c2c2c;
   }
 `;
 
+const GlobalStyles = createGlobalStyle`
+button {
+  background-color : darkred;
+  width: 85px;
+  height: 35px;
+  border-radius: 8px;
+  font-weight: 700;
+  font-size: 16px;
+  cursor : pointer;
+  transition : all 250ms linear;
+  &:hover {
+ rotate : -3deg
+  }
+}
+`;
 function StyledComponentsPage() {
   return (
-    <div className="flex items-center gap-4 p-10">
-      <Button variant="success">SUCCESS</Button>
-      <Button variant="outline">OUTLINE</Button>
-      <Button variant="danger">DANGER</Button>
-      <Button variant="warning">WARNING</Button>
-      <Button variant="info">INFO</Button>
-      <Button variant="black">BLACK</Button>
-      <Button variant="white">WHITE</Button>
-      <SecondButton variant="black">WHITE</SecondButton>
-    </div>
+    <>
+      <GlobalStyles />
+      <div className="flex items-center gap-4 p-10">
+        <Button variant="success">SUCCESS</Button>
+        <Button variant="outline">OUTLINE</Button>
+        <Button variant="danger">DANGER</Button>
+        <Button variant="warning">WARNING</Button>
+        <Button variant="info">INFO</Button>
+        <Button variant="black">BLACK</Button>
+        <Button variant="white">WHITE</Button>
+        <SecondButton href="/" variant="black">
+          WHITE
+        </SecondButton>
+        <button>CLICK ME</button>
+      </div>
+    </>
   );
 }
 
