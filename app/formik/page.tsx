d3e895d8d8235a4545ef8230febd6e0634aae7c7
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-08-18 22:54:10 +0330

    🟦 FORMIK

--- app/formik/page.tsx
+++ app/formik/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { Button } from '@/components/ui/button';
 type ErrorsType = { email: string, name: string }
 
@@ -26,9 +26,9 @@ const FormikPage = () => {
         {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
           <Form className='flex flex-col border-4 border-zinc-600 w-fit mx-auto my-16 gap-3 p-10 rounded-xl'>
             <Field type="text" name="name" placeholder="name" />
-            {errors.name && touched.name && <span className='text-xs my-0 bg-red-900 text-red-200 p-1 rounded'>{errors.name}</span>}
+            <ErrorMessage name='name' component={'span'} className='bg-red-900 text-white p-1 rounded text-[11px]' />
             <Field type="password" name="password" placeholder="password" />
-            {errors.password && touched.password && <span className='text-xs my-0 bg-red-900 text-red-200 p-1 rounded'>{errors.password}</span>}
+            <ErrorMessage name='password' component={'span'} className='bg-red-900 text-white p-1 rounded text-[11px]' />
             <Button variant={'success'} disabled={!!isSubmitting} type="submit" className='!bg-emerald-700'>SEND</Button>
           </Form>
         )}
