0bbebd860b7602c1b255fee8dc10891a11535e9d
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-08-18 22:48:30 +0330

    🟦 FORMIK

--- app/formik/page.tsx
+++ app/formik/page.tsx
@@ -9,20 +9,26 @@ const FormikPage = () => {
     <div className='section flex-col bg-black rounded-xl !mt-16 min-h-[80vh]'>
       <Formik
         initialValues={{ name: '', password: '' }}
-        onSubmit={(values) => console.info('VALUES ==>', values)}
         validate={(values) => {
-          const errors = { name: '', password: '' };
-          if (values.name.trim() == '') { errors.name = 'REQUIRED' }
-          if (values.password.trim() == '') { errors.password = "REQUIRED" }
+          const errors = {} as any;
+          if (values.name.trim() == '') { errors.name! = 'REQUIRED' }
+          if (values.password.trim() == '') { errors.password! = "REQUIRED" }
           return errors
+        }}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          console.log(values)
+          setTimeout(() => {
+            setSubmitting(false)
+          }, 400);
+          resetForm()
         }}
       >
         {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
-          <Form className='flex flex-col border-4 border-zinc-600 w-fit mx-auto my-16 gap-6 p-10 rounded-xl' onSubmit={handleSubmit}>
+          <Form className='flex flex-col border-4 border-zinc-600 w-fit mx-auto my-16 gap-3 p-10 rounded-xl'>
             <Field type="text" name="name" placeholder="name" />
-            {errors.name && <span className='text-red-600'>{errors.name}</span>}
+            {errors.name && touched.name && <span className='text-xs my-0 bg-red-900 text-red-200 p-1 rounded'>{errors.name}</span>}
             <Field type="password" name="password" placeholder="password" />
-            {errors.password && <span className='text-red-600'>{errors.password}</span>}
+            {errors.password && touched.password && <span className='text-xs my-0 bg-red-900 text-red-200 p-1 rounded'>{errors.password}</span>}
             <Button variant={'success'} disabled={!!isSubmitting} type="submit" className='!bg-emerald-700'>SEND</Button>
           </Form>
         )}
