cebf85e7c3c818d4d1c0149e6ba2f9c26e7fd64d
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-08-21 09:51:56 +0330

    REACT___HOOK___FORM

--- app/react-hook-form/practice/page.tsx
+++ app/react-hook-form/practice/page.tsx
@@ -4,22 +4,25 @@ import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Skeleton } from '@/components/ui/skeleton'
+import { cn } from '@/lib/utils'
 import { zodResolver } from '@hookform/resolvers/zod'
 import React, { useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { useForm, UseControllerProps } from 'react-hook-form'
 import toast from 'react-hot-toast'
 import z from 'zod'
+
 
 const toastStyles = { backgroundColor: 'black', color: 'white', border: '4px solid white', fontWeight: '900' }
 
 const Schema = z.object({
-  name: z.string().min(3, { error: 'MIN LENGTH IS 3' }),
+  name: z.string().min(3, { error: 'MIN LENGTH IS 3' }).nonempty({ error: 'CANT BE EMPTY' }),
   password: z.string().min(3, { error: 'MIN LENGTH IS 3' })
 })
 
-function ReactHookFormPractice() {
+function ReactHookFormPractice(props: UseControllerProps<typeof Schema>) {
 
-  const [isEdit, setIsEdit] = useState(true)
+  const [isEdit, setIsEdit] = useState(false)
+
 
   const form = useForm<z.infer<typeof Schema>>({
     resolver: zodResolver(Schema),
@@ -47,32 +50,35 @@ function ReactHookFormPractice() {
           <Form {...form}>
             <form onSubmit={form.handleSubmit(submitHandler)} className='flex flex-col gap-y-3' >
               <FormField
-                control={form.control}
+                control={form?.control}
                 name='name'
-                render={({ field }: { field: any }) => (
-                  <FormItem>
-                    <FormLabel className='font-semibold font-mono m-0 pl-1'>Username</FormLabel>
-                    <FormControl>
-                      {!field.value && isEdit ?
-                        <Skeleton className="h-9 w-44 border rounded-md" />
-                        :
-                        <Input type='text' placeholder='Username' {...field} value={field.value || ''} />
-                      }
-                    </FormControl>
-                    <FormMessage className='text-xxs text-red-900 pl-1 -translate-y-1' />
-                  </FormItem>
-                )}
+                render={({ field, fieldState }: { field: any, fieldState: any }) => {
+                  const showSkeleton = !field.value && isEdit;
+                  return (
+                    <FormItem>
+                      <FormLabel className='font-semibold font-mono m-0 pl-1'>Username</FormLabel>
+                      <FormControl>
+                        {showSkeleton ?
+                          <Skeleton className="h-9 w-[11.6rem] border rounded-md" />
+                          :
+                          <Input type='text' placeholder='Username' {...field} value={field.value || ''} />
+                        }
+                      </FormControl>
+                      <FormMessage className='text-xxs text-red-900 pl-1 -translate-y-1' />
+                    </FormItem>
+                  )
+                }}
               />
 
               <FormField
-                control={form.control}
+                control={form?.control}
                 name='password'
-                render={({ field }: { field: any }) => (
+                render={({ field, fieldState }: { field: any, fieldState: any }) => (
                   <FormItem>
                     <FormLabel className='font-semibold font-mono m-0 pl-1'>Password</FormLabel>
                     <FormControl>
                       {!field.value && isEdit ?
-                        <Skeleton className="h-9 w-44 border rounded-md" />
+                        <Skeleton className="h-9 w-[11.6rem] border rounded-md" />
                         :
                         <Input type='text' placeholder='Password' {...field} value={field.value || ''} />
                       }
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
+import { useController } from "react-hook-form"
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
