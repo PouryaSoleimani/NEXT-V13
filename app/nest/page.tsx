be08bfe53cacbee19d60ccb6e7c90207bd54dbde
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-08-21 17:38:58 +0330

    NEST

--- app/nest/page.tsx
+++ app/nest/page.tsx
@@ -14,6 +14,11 @@ function Users() {
   const deleter = (id: number) => axios.delete(`http://localhost:5000/jojos/${id}`).then(res => res.data)
   const { data, isLoading, error } = useSWR('http://localhost:5000/jojos/all/', _fetcher)
 
+  function deleteHandler(id: any) {
+    deleter(id)
+    console.info('ID =>', id)
+  }
+
   if (error) return <div>failed to load</div>
   if (isLoading) return <div>loading...</div>
 
@@ -21,13 +26,13 @@ function Users() {
     <div className='w-screen h-auto grid place-items-center-safe'>
       <Card className='p-7 my-1 grid grid-cols-4 mt-10'>
         {data.length && data?.map((item: any) => (
-          <Link href={`/users/${item.id}`} key={item.id} className='bg-black p-3 rounded-xl size-44  text-center hover:scale-105 duration-500'>
-            <CardHeader className='border-b-2 pb-2'>{item.name.toUpperCase()}</CardHeader>
+          <div key={item.id} className='bg-black p-3 rounded-xl size-44  text-center hover:scale-105 duration-500'>
+            <CardHeader className='border-b-2 pb-2'>{item.firstName.toUpperCase()}</CardHeader>
             <CardContent className='flex flex-col justify-center items-center gap-y-5 mt-6'>
               <p className={cn('text-white px-3 py-1.5 rounded-md w-32', item.role == 'front-end' ? "bg-red-800" : 'bg-blue-800')}>{item.age.toString().toUpperCase()}</p>
-              <Button variant={'destructive'} onClick={() => deleter(item.id)}>DELETE</Button>
+              <Button variant={'destructive'} onClick={() => deleteHandler(item.id)}>DELETE</Button>
             </CardContent>
-          </Link >
+          </div >
         ))}
       </Card>
     </div>
--- app/react-hook-form/form-provider/_components/PasswordInput.tsx
+++ app/react-hook-form/form-provider/_components/PasswordInput.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 import { useFormContext } from 'react-hook-form'
 
 export default function PasswordInput() {
-  const { register, formState } = useFormContext()
+  const { register, formState, watch } = useFormContext()
+  const passValue = watch('password')
   return (
     <>
       <Input type='password' {...register('password', { min: { value: 5, message: 'MINIMUM IS 5' } })} placeholder='password' />
-      {formState.errors.root && <p>{formState.errors.root.message}</p>}
+      {formState.errors.password && <p className='text-red-800 text-xs pl-1 m-0'>{formState.errors?.password.message as any}</p>}
+      {<p className='text-xs pl-1'>{passValue}</p>}
     </>
   )
 }
