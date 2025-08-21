c570521703b6834269e8bc451ffb81476e1105ea
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-08-21 10:28:11 +0330

    REACT___HOOK___FORM

---
+++ app/react-hook-form/form-provider/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import React, { useEffect } from 'react'
+import { useForm, FormProvider, useFormContext } from "react-hook-form"
+
+function MyFormProvider() {
+
+  const methods = useForm()
+  const { register, reset } = methods
+
+  const onSubmit = (data: any) => { console.log(data); reset() }
+
+
+  return (
+    <FormProvider {...methods}>
+      <div className='w-screen h-screen grid place-items-center'>
+        <Card className='p-4'>
+          <form onSubmit={methods.handleSubmit(onSubmit)} className='space-y-3'>
+            <Input {...register("name")} placeholder='name' />
+            <NestedInput />
+            <Button type="submit" variant={'blue'} className='mt-4 block w-full'>SUBMIT</Button>
+          </form>
+        </Card>
+      </div>
+    </FormProvider >
+  )
+}
+
+export default MyFormProvider
+
+function NestedInput() {
+  const { register } = useFormContext();
+  return <Input {...register("data")} placeholder='data' />;
+}
+
+
