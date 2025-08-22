d0a761cf2d1c8f545f8087efe0ddbc271ebe258d
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-08-22 18:28:56 +0330

    🟧 REACT___QUERY

--- app/react-query/_hooks/useFetchUsers.tsx
+++ app/react-query/_hooks/useFetchUsers.tsx
@@ -6,22 +6,17 @@ const useFetchUsers = (page: number) => {
   return useQuery({
     queryKey: ['repoData'],
     queryFn: () => fetch(`http://localhost:5000/users?_limit=3&_page=${page}`).then((res) => res.json()),
-    staleTime: 5000,
-    refetchOnMount: true,
-    refetchOnWindowFocus: true,
-    refetchInterval: 5000,
-    refetchOnReconnect: true,
+    // staleTime: 5000,
+    // refetchOnMount: true,
+    // refetchOnWindowFocus: true,
+    // refetchInterval: 5000,
+    // refetchOnReconnect: true,
     select: (data) => {
       return data.map((item: any) => ({
         ...item,
         name: item.name.toUpperCase(),
         age: '$' + (item.age * 1000).toLocaleString(),
       }));
-    },
-    initialData: () => {
-      const users = QueryClient?.getQueryData(['repoData']);
-      console.info(users);
-      return users;
     },
   });
 };
