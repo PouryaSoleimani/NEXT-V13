7a3808eaec55f32443ec489e71a9ca56089ac398
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-08-22 08:03:09 +0330

    🟧 REACT___QUERY

--- app/react-query/page.tsx
+++ app/react-query/page.tsx
@@ -5,8 +5,26 @@ const queryClient = new QueryClient()
 
 
 const ReactQuery = () => {
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () => fetch('https://api.github.com/repos/TanStack/query').then((res) => res.json(),),
+  })
+
+  if (isPending) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+
   return (
-    <div>ReactQuery</div>
+    <section className='screen bg-black'>
+      <div>
+        <h1>{data.name}</h1>
+        <p>{data.description}</p>
+        <strong>👀 {data.subscribers_count}</strong>{' '}
+        <strong>✨ {data.stargazers_count}</strong>{' '}
+        <strong>🍴 {data.forks_count}</strong>
+      </div>
+    </section>
   )
 }
 
