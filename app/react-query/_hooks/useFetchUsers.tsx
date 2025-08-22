42c8dc8176e243bb6361ec178b24581a3325be2e
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-08-22 11:32:07 +0330

    🟧 REACT___QUERY

--- app/react-query/_hooks/useFetchUsers.tsx
+++ app/react-query/_hooks/useFetchUsers.tsx
@@ -1,17 +1,23 @@
-import { useQuery } from '@tanstack/react-query'
-import React from 'react'
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
 
 const useFetchUsers = () => {
-
   return useQuery({
     queryKey: ['repoData'],
-    queryFn: () => fetch('http://localhost:5000/users').then((res) => res.json(),),
+    queryFn: () => fetch('http://localhost:5000/users').then((res) => res.json()),
     staleTime: 5000,
     refetchOnMount: true,
     refetchOnWindowFocus: true,
     refetchInterval: 5000,
     refetchOnReconnect: true,
-  })
-}
+    select: (data) => {
+      return data.map((item: any) => ({
+        ...item,
+        name: item.name.toUpperCase(),
+        age: '$ ' + (item.age * 1000).toLocaleString(),
+      }));
+    },
+  });
+};
 
-export default useFetchUsers
+export default useFetchUsers;
--- app/react-query/page.tsx
+++ app/react-query/page.tsx
@@ -1,35 +1,38 @@
-import React from 'react'
-import { useQuery } from '@tanstack/react-query'
-import { Card } from '@/components/ui/card'
-import { LoaderCircle } from 'lucide-react'
-import { BiError } from 'react-icons/bi'
-import useFetchUsers from './_hooks/useFetchUsers'
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { Card } from '@/components/ui/card';
+import { LoaderCircle } from 'lucide-react';
+import { BiError } from 'react-icons/bi';
+import useFetchUsers from './_hooks/useFetchUsers';
 
 const ReactQuery = () => {
+  const { isLoading, error, data } = useFetchUsers();
 
-  const { isLoading, error, data } = useFetchUsers()
+  if (isLoading)
+    return (
+      <div className="w-screen h-screen flex flex-col text-xl font-mono gap-2 justify-center items-center">
+        <LoaderCircle className="size-12 animate-spin stroke-orange-500" />
+        Loading ...
+      </div>
+    );
 
-  if (isLoading) return (
-    <div className='w-screen h-screen flex flex-col text-xl font-mono gap-2 justify-center items-center'>
-      <LoaderCircle className='size-12 animate-spin stroke-orange-500' />
-      Loading ...
-    </div>
-  )
-
-  if (error) return (
-    <div className='w-screen h-screen flex flex-col text-xl font-mono gap-2 justify-center items-center'>
-      <BiError className='size-12 text-red-500' />
-      Error : {error.message}
-    </div>
-  )
+  if (error)
+    return (
+      <div className="w-screen h-screen flex flex-col text-xl font-mono gap-2 justify-center items-center">
+        <BiError className="size-12 text-red-500" />
+        Error : {error.message}
+      </div>
+    );
 
   return (
     <>
-      <section className='h-screen  bg-black'>
-        <h2 className='text-orange-500 text-4xl text-center font-black relative top-10 left-0 right-0'>REACT___QUERY</h2>
-        <div className='flex mt-16 px-10 gap-3'>
+      <section className="h-screen  bg-black">
+        <h2 className="text-orange-500 text-4xl text-center font-black relative top-10 left-0 right-0">
+          REACT___QUERY
+        </h2>
+        <div className="flex mt-16 px-10 gap-3">
           {data.map((item: any) => (
-            <Card key={item.id} className='p-5 text-xl font-mono *:rounded-lg w-36'>
+            <Card key={item.id} className="p-5 text-xl font-mono *:rounded-lg w-36">
               <h1>{item.name}</h1>
               <p>{item.age}</p>
             </Card>
@@ -37,7 +40,7 @@ const ReactQuery = () => {
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default ReactQuery  
+export default ReactQuery;
