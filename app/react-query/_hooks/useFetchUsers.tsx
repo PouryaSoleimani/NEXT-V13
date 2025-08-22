17d6439c4614e890007e374b7cadb2b360a35fcc
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-08-22 18:16:29 +0330

    🟧 REACT___QUERY

--- app/react-query/_hooks/useFetchUsers.tsx
+++ app/react-query/_hooks/useFetchUsers.tsx
@@ -1,11 +1,11 @@
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import React from 'react';
 
-const useFetchUsers = () => {
+const useFetchUsers = (page: number) => {
   const QueryClient = useQueryClient();
   return useQuery({
     queryKey: ['repoData'],
-    queryFn: () => fetch('http://localhost:5000/users').then((res) => res.json()),
+    queryFn: () => fetch(`http://localhost:5000/users?_limit=3&_page=${page}`).then((res) => res.json()),
     staleTime: 5000,
     refetchOnMount: true,
     refetchOnWindowFocus: true,
--- app/react-query/page.tsx
+++ app/react-query/page.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { Card } from '@/components/ui/card';
 import { LoaderCircle } from 'lucide-react';
 import { BiError } from 'react-icons/bi';
 import useFetchUsers from './_hooks/useFetchUsers';
 import { Button } from '@/components/ui/button';
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious, } from '@/components/ui/pagination';
 
 const ReactQuery = () => {
-  const { isLoading, error, data, refetch } = useFetchUsers();
+  const [page, setPage] = useState(1);
+  const { isLoading, error, data, refetch } = useFetchUsers(page);
 
   if (isLoading)
     return (
@@ -31,17 +34,44 @@ const ReactQuery = () => {
         <h2 className="text-orange-500 text-4xl text-center font-black relative top-10 left-0 right-0">
           REACT___QUERY
         </h2>
-        <div className="flex mt-16 px-10 gap-3">
-          {data.map((item: any) => (
-            <Card key={item.id} className="p-5 text-xl font-mono *:rounded-lg w-36">
-              <h1>{item.name}</h1>
-              <p>{item.age}</p>
-            </Card>
-          ))}
+        <div className="flex flex-col justify-center-safe  mt-16 px-10 gap-6">
+          <div className='flex items-center-safe justify-center-safe gap-3'>
+            {data.map((item: any) => (
+              <Card key={item.id} className="p-5 text-xl font-mono *:rounded-lg w-36">
+                <h1>{item.name}</h1>
+                <p>{item.age}</p>
+              </Card>
+            ))}
+          </div>
+
+          <Pagination>
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious href="#" />
+              </PaginationItem>
+              <PaginationItem>
+                <Button onClick={() => setPage(1)}>1</Button>
+              </PaginationItem>
+              <PaginationItem>
+                <Button onClick={() => setPage(2)}>2</Button>
+              </PaginationItem>
+              <PaginationItem>
+                <Button onClick={() => setPage(3)}>3</Button>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+              <PaginationItem>
+                <Button variant={'ghost'} onClick={() => setPage(prev => prev - 1)} >Next {'>'}</Button>
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
         </div>
-        <Button variant={'outline'} onClick={() => refetch()} className="m-5">
-          REFETCH
-        </Button>
+        <div className="flex items-center justify-center-safe">
+          <Button variant={'outline'} onClick={() => refetch()} className="m-5">
+            REFETCH
+          </Button>
+        </div>
       </section>
     </>
   );
