eefe84555058f4250b2fcb22075914a08e0b9935
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-08-31 16:44:49 +0330

    DND_____KIT

--- app/dnd-kit-2/_components/Task.tsx
+++ app/dnd-kit-2/_components/Task.tsx
@@ -10,9 +10,10 @@ function Task({ id, title }: { id: number, title: string }) {
     transition,
     transform: CSS.Transform.toString(transform)
   }
+
   return (
     <>
-      <div style={style} ref={setNodeRef} {...attributes} {...listeners} className='bg-white text-black font-mono font-black tracking-tight shadow-xs shadow-neutral-600 text-lg p-3 rounded-md flex items-center gap-3 cursor-pointer touch-none hover:border-2 '>
+      <div style={style} ref={setNodeRef} {...attributes} {...listeners} className='bg-white text-black box-border font-mono font-black tracking-tight shadow-xs shadow-neutral-600 text-lg p-3 rounded-md flex items-center gap-3 cursor-pointer touch-none hover:shadow-md '>
         {title}
       </div>
     </>
