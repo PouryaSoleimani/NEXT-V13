e88a619e18cb862af558c53b73223539b05bc60e
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-09-03 16:26:34 +0330

    DND___TEST

--- app/dnd-kit-5/page.tsx
+++ app/dnd-kit-5/page.tsx
@@ -12,8 +12,15 @@ const DndKitPage5 = () => {
       { id: 1, title: "LEARN__JS" },
       { id: 2, title: "LEARN___TS" },
       { id: 3, title: "LEARN___REACT" },
-      { id: 4, title: "LEARN___NEXT" },
-      { id: 5, title: "LEARN___NEST" },
+      { id: 4, title: "LEARN___REACT___LIBRARIES" },
+      { id: 5, title: "LEARN___NEXT" },
+   ]);
+
+   const [tasks2, setTasks2] = useState([
+      { id: 6, title: "LEARN__NODE__JS" },
+      { id: 7, title: "LEARN___MONGO___DB" },
+      { id: 8, title: "LEARN___NEST___JS" },
+      { id: 9, title: "LEARN___PRISMA" },
    ]);
 
    const sensors = useSensors(
@@ -24,6 +31,10 @@ const DndKitPage5 = () => {
 
    function getRowPosition(id: number) {
       return tasks.findIndex((item: SingleTaskType) => item.id === id);
+   }
+
+   function getRowPosition2(id: number) {
+      return tasks2.findIndex((item: SingleTaskType) => item.id === id);
    }
 
    function handleDragEnd(event: any) {
@@ -37,10 +48,25 @@ const DndKitPage5 = () => {
       });
    }
 
+   function handleDragEnd2(event: any) {
+      const { active, over } = event;
+      if (active.id === over.id) return;
+      const originalPos = getRowPosition2(active.id);
+      const newPos = getRowPosition2(over.id);
+
+      setTasks2((tasks2: SingleTaskType[]) => {
+         return arrayMove(tasks2, originalPos, newPos);
+      });
+   }
+
    return (
       <div className="screen center bg-stone-600">
          <DndContext sensors={sensors} collisionDetection={closestCenter} modifiers={[restrictToVerticalAxis]} onDragEnd={handleDragEnd}>
             <Column tasks={tasks} />
+         </DndContext>
+
+         <DndContext sensors={sensors} collisionDetection={closestCenter} modifiers={[restrictToVerticalAxis]} onDragEnd={handleDragEnd2}>
+            <Column tasks={tasks2} />
          </DndContext>
       </div>
    );
