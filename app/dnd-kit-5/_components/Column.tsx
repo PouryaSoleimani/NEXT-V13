75f9621de29dbe24cab6d8f4b0633e4b4bcbe990
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-09-03 12:04:46 +0330

    DND____TEST

--- app/dnd-kit-5/_components/Column.tsx
+++ app/dnd-kit-5/_components/Column.tsx
@@ -1,14 +1,17 @@
+import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
 import React from "react";
 type ColumnPropsType = { tasks: { id: number; title: string }[] };
 
 const Column: React.FC<ColumnPropsType> = ({ tasks }) => {
    return (
       <div className="bg-stone-300 w-1/4 p-5 flex flex-col gap-2 rounded-lg shadow-sm shadow-stone-300">
-         {tasks.map((task: any) => (
-            <p className="bg-white text-black p-3 rounded-lg shadow-sm shadow-stone-300" key={task.id}>
-               {task.title}
-            </p>
-         ))}
+         <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
+            {tasks.map((task: any) => (
+               <p className="bg-white text-black p-3 rounded-lg shadow-sm shadow-stone-300" key={task.id}>
+                  {task.title}
+               </p>
+            ))}
+         </SortableContext>
       </div>
    );
 };
