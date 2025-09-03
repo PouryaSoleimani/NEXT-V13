b7b84e57df420710cd0ce42fa86077c384a155c2
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2025-09-03 20:16:52 +0330

    DND_____KIT___LAYOUT

--- app/dnd-kit-4/_components/Column.tsx
+++ app/dnd-kit-4/_components/Column.tsx
@@ -3,15 +3,15 @@ import React from "react";
 import Task from "./Task";
 
 const Column = ({ tasks }: { tasks: any }) => {
-  return (
-    <div className="bg-stone-700 w-120 p-4 rounded-md border-2 border-black shadow-sm shadow-black">
-      <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
-        {tasks.map((item: { id: number; title: string; icon: string }) => (
-          <Task {...item} />
-        ))}
-      </SortableContext>
-    </div>
-  );
+   return (
+      <div className="bg-stone-700 w-120 p-4 rounded-md border-2 border-black shadow-sm shadow-black">
+         <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
+            {tasks.map((item: { id: number; title: string; icon: string }) => (
+               <Task {...item} />
+            ))}
+         </SortableContext>
+      </div>
+   );
 };
 
 export default Column;
