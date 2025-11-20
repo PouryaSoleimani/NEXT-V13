eecedd2930d0c4376af21cc48430ac63d10d2b66
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-11-20 09:55:17 +0330

    ⬛ SHAD____CN

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -1,8 +1,10 @@
+'use client'
 import { NextPage } from 'next'
-import React from 'react'
+import React, { useState } from 'react'
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger, } from "@/components/ui/accordion"
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger, } from "@/components/ui/alert-dialog"
-import { Button } from "@/components/ui/button"
+import { CheckCircle2Icon } from "lucide-react"
+import { Alert, AlertDescription, AlertTitle, } from "@/components/ui/alert"
 
 // TYPES ========================================================================================================================================================================================================================
 interface PropsType {
@@ -12,8 +14,18 @@ interface PropsType {
 
 // COMPONENT ========================================================================================================================================================================================================================
 const ReviewPage: NextPage<PropsType> = () => {
+  // STATES
+  const [isShowAlert, setIsShowAlert] = useState(false)
+  // FUNCTIONS
+  function alertShowHandler() {
+    setIsShowAlert(true)
+    setTimeout(() => {
+      setIsShowAlert(false)
+    }, 1500);
+  }
+  // RETURN 
   return (
-    <section className='w-screen h-screen center flex-col gap-4'>
+    <section className='w-screen h-screen center flex-col gap-4 relative'>
       {/* ACCORDION */}
       <Accordion type="single" collapsible className='border-2 border-zinc-800 rounded-xl px-4 min-w-82 gap-y-2 transition-all duration-300'>
         <AccordionItem value="item-1">
@@ -42,10 +54,20 @@ const ReviewPage: NextPage<PropsType> = () => {
           </AlertDialogHeader>
           <AlertDialogFooter>
             <AlertDialogCancel className='bg-red-900 border-red-950 cursor-pointer'>Cancel</AlertDialogCancel>
-            <AlertDialogAction className='bg-zinc-200 border-zinc-400 text-zinc-900 hover:text-white hover:bg-emerald-900 cursor-pointer'>Continue</AlertDialogAction>
+            <AlertDialogAction onClick={alertShowHandler} className='bg-zinc-200 border-zinc-400 text-zinc-900 hover:text-white hover:bg-emerald-900 cursor-pointer'>Continue</AlertDialogAction>
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
+      {/* ALERT */}
+      {isShowAlert &&
+        <Alert className='absolute right-10 bottom-30  w-fit bg-emerald-900/30 border-emerald-900 text-zinc-200 leading-8'>
+          <CheckCircle2Icon className='!size-7 -translate-x-2 !translate-y-3.5' />
+          <AlertTitle>Success! Your changes have been saved</AlertTitle>
+          <AlertDescription>
+            This is an alert with icon, title and description.
+          </AlertDescription>
+        </Alert>
+      }
     </section>
   )
 }
