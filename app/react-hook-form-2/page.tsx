2de3ed96c15f3f99d524ccf7ba8a6f3037046353
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-10 11:49:12 +0330

    REACT__HOOK__FORM__REVIEW

--- app/react-hook-form-2/page.tsx
+++ app/react-hook-form-2/page.tsx
@@ -12,19 +12,14 @@ const ReactHookForm = () => {
          setError("EMAIL IS REQUIRED");
          return;
       }
-      setEmail("");
-      alert(`EMAIL SUBMITTED : ${email}`);
+      if (!error) {
+         setEmail("");
+         alert(`EMAIL SUBMITTED : ${email}`);
+      }
    }
    return (
       <form className="bg-zinc-700 w-fit p-6 rounded-xl mx-auto my-32 flex flex-col gap-2" onSubmit={handleSubmit}>
-         <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="EMAIL ..."
-            disabled={!!error.length}
-            className="border border-black p-1 rounded-lg"
-         />
+         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="EMAIL ..." className="border border-black p-1 rounded-lg" />
          {error && <p className="text-sm text-red-700 font-sans">{error}</p>}
          <Button>SUBMIT</Button>
       </form>
