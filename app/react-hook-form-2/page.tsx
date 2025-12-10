744c671cab9250699a18124e72b9b95cb4ebaa8a
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-10 12:37:01 +0330

    REACT__HOOK__FORM__REVIEW

--- app/react-hook-form-2/page.tsx
+++ app/react-hook-form-2/page.tsx
@@ -12,19 +12,20 @@ const FormSchema = z.object({
 type FormValuesType = z.infer<typeof FormSchema>;
 
 const ReactHookFormPage = () => {
-   const { register, handleSubmit, formState, reset } = useForm<FormValuesType>({
+   const { register, handleSubmit, formState, reset, setError } = useForm<FormValuesType>({
       resolver: zodResolver(FormSchema),
    });
 
    function onSubmit(data: FormValuesType, e: any) {
       console.log("DATAS => ", data);
       alert(`FORM VALUES => EMAIL : ${data.email}`);
+      setError("email", { message: "" });
       reset();
    }
 
    return (
       <form className="bg-zinc-700 w-fit p-6 rounded-xl mx-auto my-32 flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
-         <input type="email" placeholder="EMAIL ..." {...register("email", { required: "Email is Required" })} className="p-1 border border-black rounded-lg" />
+         <input type="email" placeholder="EMAIL ..." {...register("email", { required: "Email is Required" })} className="p-2 border border-black rounded-lg font-thin" />
          {formState.isDirty && formState.errors.email && <p className="text-sm text-red-700 font-sans">{formState.errors.email.message}</p>}
          <Button type="submit">SUBMIT</Button>
       </form>
