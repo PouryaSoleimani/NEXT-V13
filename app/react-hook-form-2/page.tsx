6ad75db6c5a413b2366e50c5935eddc50b124466
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-10 13:44:46 +0330

    REACT__HOOK__FORM__REVIEW

--- app/react-hook-form-2/page.tsx
+++ app/react-hook-form-2/page.tsx
@@ -24,9 +24,18 @@ const ReactHookFormPage = () => {
    }
 
    return (
-      <form className="bg-zinc-700 w-fit p-6 rounded-xl mx-auto my-32 flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
-         <input type="email" placeholder="EMAIL ..." {...register("email", { required: "Email is Required" })} className="p-2 border border-black rounded-lg font-thin" />
-         {formState.isDirty && formState.errors.email && <p className="text-sm text-red-700 font-sans">{formState.errors.email.message}</p>}
+      <form
+         className="bg-zinc-700 w-fit p-6 rounded-xl mx-auto my-32 flex flex-col gap-2"
+         onSubmit={handleSubmit(onSubmit)}>
+         <input
+            type="email"
+            placeholder="EMAIL ..."
+            {...register("email", { required: "Email is Required" })}
+            className="p-2 border border-black rounded-lg font-thin"
+         />
+         {formState.isDirty && formState.errors.email && (
+            <p className="text-sm text-red-700 font-sans">{formState.errors.email.message}</p>
+         )}
          <Button type="submit">SUBMIT</Button>
       </form>
    );
