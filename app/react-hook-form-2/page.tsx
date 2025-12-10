35ace992a9a14fd0a4bb381b6e3d01ebd6c64198
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-10 21:19:29 +0330

    RHF

--- app/react-hook-form-2/page.tsx
+++ app/react-hook-form-2/page.tsx
@@ -62,95 +62,100 @@ const ReactHookFormPage = () => {
    }
 
    return (
-      <form
-         className="bg-zinc-700/50 backdrop-blur-2xl border shadow-sm shadow-zinc-500 border-zinc-500 w-100  p-6 rounded-xl mx-auto my-32 flex flex-col gap-2"
-         onSubmit={handleSubmit(onSubmit)}>
-         {/* EMAIL */}
-         <Label>Email</Label>
-         <input
-            type="email"
-            placeholder="Email ..."
-            {...register("email")}
-            className="p-2 border border-black rounded-lg font-thin"
-         />
-         {formState.errors.email && (
-            <p className="text-xs tracking-wide text-red-300 font-sans">{formState.errors.email.message}</p>
-         )}
-         {/* NAME */}
-         <Label className="mt-2">Username</Label>
-         <input
-            type="text"
-            placeholder="Username ..."
-            className="p-2 border border-black rounded-lg font-thin"
-            {...register("username")}
-         />
-         {formState.errors.username && (
-            <p className="text-xs tracking-wide text-red-300 font-sans pt-1">{formState.errors.username.message}</p>
-         )}
+      <div className="w-full h-full bg-black flex items-center justify-center">
+         <form
+            className="bg-zinc-700/50 backdrop-blur-2xl border shadow-sm shadow-zinc-500 border-zinc-500 w-100  p-6 rounded-xl mx-auto flex flex-col gap-2"
+            onSubmit={handleSubmit(onSubmit)}>
+            {/* EMAIL */}
+            <Label>Email</Label>
+            <input
+               type="email"
+               placeholder="Email ..."
+               {...register("email")}
+               className="p-2 border border-black rounded-lg font-thin"
+            />
+            {formState.errors.email && (
+               <p className="text-xs tracking-wide text-red-300 font-sans">{formState.errors.email.message}</p>
+            )}
+            {/* NAME */}
+            <Label className="mt-2">Username</Label>
+            <input
+               type="text"
+               placeholder="Username ..."
+               className="p-2 border border-black rounded-lg font-thin"
+               {...register("username")}
+            />
+            {formState.errors.username && (
+               <p className="text-xs tracking-wide text-red-300 font-sans pt-1">{formState.errors.username.message}</p>
+            )}
 
-         {/* PASSWORD */}
-         <div className="relative inset-0 mt-2">
-            <Label className="pb-2">Password</Label>
-            <input
-               type={type == "password" ? "password" : "text"}
-               placeholder="Password ..."
-               {...register("password")}
-               className="p-2 border w-full border-black rounded-lg font-thin"
-            />
-            <Eye onClick={switchShowPassword} className="absolute bottom-2 right-3 text-zinc-300" />
-         </div>
-         {formState.errors.password && (
-            <p className="text-xs tracking-wide text-red-300 font-sans pt-1 max-w-sm">
-               {formState.errors.password.message}
-            </p>
-         )}
-         {/* CONFIRM PASSWORD */}
-         <div className="relative inset-0 mt-2">
-            <Label>Confirm Password</Label>
-            <input
-               type={type == "password" ? "password" : "text"}
-               placeholder="Confirm Password ..."
-               {...register("confirmPassword")}
-               className="p-2 border border-black rounded-lg mt-2 font-thin w-full"
-            />
-            {formState.errors.confirmPassword && (
-               <p className="text-xs tracking-wide text-red-300 font-sans pt-1">
-                  {formState.errors.confirmPassword.message}
+            {/* PASSWORD */}
+            <div className="relative inset-0 mt-2">
+               <Label className="pb-2">Password</Label>
+               <input
+                  type={type == "password" ? "password" : "text"}
+                  placeholder="Password ..."
+                  {...register("password")}
+                  className="p-2 border w-full border-black rounded-lg font-thin"
+               />
+               <Eye onClick={switchShowPassword} className="absolute bottom-2 right-3 text-zinc-300" />
+            </div>
+            {formState.errors.password && (
+               <p className="text-xs tracking-wide text-red-300 font-sans pt-1 max-w-sm">
+                  {formState.errors.password.message}
                </p>
             )}
-            <Eye
-               onClick={switchShowPassword}
-               className={cn("absolute bottom-2 right-3 text-zinc-300", formState.errors.confirmPassword && "bottom-7")}
-            />
-         </div>
-
-         {/* HAS PHONE NUMBER */}
-         <div className="flex  items-center-safe gap-3  border-t-2 border-zinc-600 pt-3">
-            <input type="checkbox" {...register("hasPhone")} />
-            <Label className="mt-0.5">Has PhoneNumber ?</Label>
-            <p>{watch("hasPhone")?.toString()}</p>
-         </div>
-         {watch("hasPhone") && (
+            {/* CONFIRM PASSWORD */}
             <div className="relative inset-0 mt-2">
-               <Label>Phone Number</Label>
+               <Label>Confirm Password</Label>
                <input
-                  type="number"
-                  placeholder="PhoneNumber"
-                  {...register("phonenumber")}
+                  type={type == "password" ? "password" : "text"}
+                  placeholder="Confirm Password ..."
+                  {...register("confirmPassword")}
                   className="p-2 border border-black rounded-lg mt-2 font-thin w-full"
                />
-               {formState.errors.phonenumber && (
+               {formState.errors.confirmPassword && (
                   <p className="text-xs tracking-wide text-red-300 font-sans pt-1">
-                     {formState.errors.phonenumber.message}
+                     {formState.errors.confirmPassword.message}
                   </p>
                )}
+               <Eye
+                  onClick={switchShowPassword}
+                  className={cn(
+                     "absolute bottom-2 right-3 text-zinc-300",
+                     formState.errors.confirmPassword && "bottom-7"
+                  )}
+               />
             </div>
-         )}
 
-         <Button type="submit" variant={"success"} className="mt-3 font-thin">
-            SUBMIT
-         </Button>
-      </form>
+            {/* HAS PHONE NUMBER */}
+            <div className="flex  items-center-safe gap-3  border-t-2 border-zinc-600 pt-3">
+               <input type="checkbox" {...register("hasPhone")} />
+               <Label className="mt-0.5">Has PhoneNumber ?</Label>
+               <p>{watch("hasPhone")?.toString()}</p>
+            </div>
+            {watch("hasPhone") && (
+               <div className="relative inset-0 mt-2">
+                  <Label>Phone Number</Label>
+                  <input
+                     type="number"
+                     placeholder="PhoneNumber"
+                     {...register("phonenumber")}
+                     className="p-2 border border-black rounded-lg mt-2 font-thin w-full"
+                  />
+                  {formState.errors.phonenumber && (
+                     <p className="text-xs tracking-wide text-red-300 font-sans pt-1">
+                        {formState.errors.phonenumber.message}
+                     </p>
+                  )}
+               </div>
+            )}
+
+            <Button type="submit" variant={"success"} className="mt-3 font-thin">
+               SUBMIT
+            </Button>
+         </form>
+      </div>
    );
 };
 
