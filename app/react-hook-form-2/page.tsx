bfc7ecde94da71f404af35db336b508be987e3ae
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-10 11:37:42 +0330

    REACT__HOOK__FORM__REVIEW

--- app/react-hook-form-2/page.tsx
+++ app/react-hook-form-2/page.tsx
@@ -1,8 +1,19 @@
 "use client";
-import React from "react";
+import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
 
 const ReactHookForm = () => {
-   return <div>ReactHookForm</div>;
+   const [email, setEmail] = useState("");
+   const [error, setError] = useState("");
+   function handleSubmit(e: any) {
+      
+   }
+   return (
+      <form>
+         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="EMAIL ..." disabled={!!error.length} />
+         <Button>SUBMIT</Button>
+      </form>
+   );
 };
 
 export default ReactHookForm;
