22dd2ddae0da4509653ba102ecbd6876220e0241
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-10 11:47:49 +0330

    REACT__HOOK__FORM__REVIEW

--- app/react-hook-form-2/page.tsx
+++ app/react-hook-form-2/page.tsx
@@ -5,12 +5,27 @@ import React, { useState } from "react";
 const ReactHookForm = () => {
    const [email, setEmail] = useState("");
    const [error, setError] = useState("");
+
    function handleSubmit(e: any) {
-      
+      e.preventDefault();
+      if (!email) {
+         setError("EMAIL IS REQUIRED");
+         return;
+      }
+      setEmail("");
+      alert(`EMAIL SUBMITTED : ${email}`);
    }
    return (
-      <form>
-         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="EMAIL ..." disabled={!!error.length} />
+      <form className="bg-zinc-700 w-fit p-6 rounded-xl mx-auto my-32 flex flex-col gap-2" onSubmit={handleSubmit}>
+         <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="EMAIL ..."
+            disabled={!!error.length}
+            className="border border-black p-1 rounded-lg"
+         />
+         {error && <p className="text-sm text-red-700 font-sans">{error}</p>}
          <Button>SUBMIT</Button>
       </form>
    );
