cb19cd9ed47739a7db6305699bc922300b893112
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-11 10:44:01 +0330

    REACT__HOOK__FORM__REVIEW

--- components/AxiosProducts.tsx
+++ components/AxiosProducts.tsx
@@ -1,31 +1,28 @@
 "use client";
 import React from "react";
 interface Props {
-  data: { id: number; title: string }[];
+   data: { id: number; title: string }[];
 }
 
 const AxiosProducts = (data: Props) => {
-  console.log("ENV", process.env.NEXT_PUBLIC_NAME);
+   console.log("ENV", process.env.NEXT_PUBLIC_NAME);
 
-  return (
-    <div className="px-6">
-      <h2 className="text-center text-3xl font-black w-fit mx-auto px-8 py-4 rounded-xl bg-purple-700">
-        AxiosProducts
-      </h2>
-      {data.data.length > 0 ? (
-        data.data.map((item) => (
-          <h2
-            key={item.id}
-            className="bg-gray-800 text-white py-4 my-4 px-4 rounded w-fit font-black text-2xl "
-          >
-            {item.title}
-          </h2>
-        ))
-      ) : (
-        <div>No Data</div>
-      )}
-    </div>
-  );
+   return (
+      <div className="px-6">
+         <h2 className="text-center text-3xl font-black w-fit mx-auto px-8 py-4 rounded-xl bg-purple-700">
+            AxiosProducts
+         </h2>
+         {data.data.length > 0 ? (
+            data.data.map((item) => (
+               <h2 key={item.id} className="bg-gray-800 text-white py-4 my-4 px-4 rounded w-fit font-black text-2xl ">
+                  {item.title}
+               </h2>
+            ))
+         ) : (
+            <div>No Data</div>
+         )}
+      </div>
+   );
 };
 
 export default AxiosProducts;
