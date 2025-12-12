81b65527655c35e414b3593770c8c522fbce79a2
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-12 18:17:49 +0330

    RHF

--- app/react-hook-form-2/v2/page.tsx
+++ app/react-hook-form-2/v2/page.tsx
@@ -4,10 +4,11 @@ import { Label } from "@/components/ui/label";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Eye } from "lucide-react";
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import z from "zod";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue, } from "@/components/ui/select";
-
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import toast from "react-hot-toast";
+enum GENDER {male ='MALE' , female ='FEMALE'}
 
 const FormSchema = z
    .object({
@@ -21,6 +22,7 @@ const FormSchema = z
          .regex(/^\d+$/, "Phonenumber must be only Digits")
          .min(7, "phonenumber must be at least 7 letters")
          .optional(),
+      gender: z.enum(GENDER, "Please Select a Gender "),
    })
    .refine((data) => data.password === data.confirmPassword, {
       message: "Passwords don't Match ...",
@@ -63,26 +65,32 @@ const ReactHookFormV2 = () => {
 
    type FormTypes = z.infer<typeof FormSchema>;
 
-   const { register, formState, handleSubmit, watch, trigger } = useForm<FormTypes>({
+   const { register, formState, handleSubmit, watch, control, reset } = useForm<FormTypes>({
       resolver: zodResolver(FormSchema),
    });
 
    function submitHandler(data: FormTypes) {
       console.info(data);
+      toast.loading("SUBMITTING FORM");
+      setTimeout(() => {
+         toast.dismiss();
+         toast.success("FORM SUBMITTED SUCCESFULLY", { position: "top-center" });
+         reset();
+      }, 1500);
    }
 
    return (
       <div className="w-screen h-screen center bg-black">
          <form
             onSubmit={handleSubmit(submitHandler)}
-            className="bg-neutral-800 border p-4 rounded-lg w-100  flex flex-col gap-5 border-cyan-500">
-            <div className="flex flex-col gap-2">
+            className="bg-neutral-800 border p-4 rounded-lg w-100  flex flex-col gap-5 border-neutral-500">
+               
+            <div id="USER__NAME" className="flex flex-col gap-2">
                <Label>Username</Label>
                <input
                   {...register("username")}
                   placeholder="username"
                   type="text"
-                  onChange={() => trigger("username")}
                   className="border-neutral-700 px-2 py-1.5 rounded-lg ring-neutral-700 bg-neutral-400 text-black"
                />
                {formState.errors.username && (
@@ -92,7 +100,7 @@ const ReactHookFormV2 = () => {
                )}
             </div>
 
-            <div className="flex flex-col gap-2">
+            <div id="EMAIL" className="flex flex-col gap-2">
                <Label>Email</Label>
                <input
                   {...register("email")}
@@ -107,7 +115,7 @@ const ReactHookFormV2 = () => {
                )}
             </div>
 
-            <div className="flex flex-col gap-2 relative inset-0">
+            <div id="PASSWORD" className="flex flex-col gap-2 relative inset-0">
                <Label>Password</Label>
                <input
                   {...register("password")}
@@ -120,10 +128,13 @@ const ReactHookFormV2 = () => {
                      {formState.errors.password.message}
                   </p>
                )}
-               <Eye className="absolute top-7.5 right-2 size-5 text-neutral-800" onClick={switchShowPassword} />
+               <Eye
+                  className="absolute top-7.5 right-2 size-5 text-neutral-800"
+                  onClick={switchShowPassword}
+               />
             </div>
 
-            <div className="flex flex-col gap-2 relative inset-0">
+            <div id="CONFIRM__PASSWORD" className="flex flex-col gap-2 relative inset-0">
                <Label>Confirm Password</Label>
                <input
                   {...register("confirmPassword")}
@@ -136,16 +147,19 @@ const ReactHookFormV2 = () => {
                      {formState.errors.confirmPassword.message}
                   </p>
                )}
-               <Eye className="absolute top-7.5 right-2 size-5 text-neutral-800" onClick={switchShowPassword2} />
+               <Eye
+                  className="absolute top-7.5 right-2 size-5 text-neutral-800"
+                  onClick={switchShowPassword2}
+               />
             </div>
 
-            <div className="flex items-center-safe gap-2">
+            <div id="HAS__PHONE__NUMBER" className="flex items-center-safe gap-2">
                <input type="checkbox" {...register("hasPhone")} />
                <Label>Has Phonenumber</Label>
             </div>
 
             {watch("hasPhone") && (
-               <div className="flex flex-col gap-2 relative inset-0">
+               <div id="PHONE____NUMBER" className="flex flex-col gap-2 relative inset-0">
                   <Label>Phonenumber</Label>
                   <input
                      {...register("phoneNumber")}
@@ -160,19 +174,40 @@ const ReactHookFormV2 = () => {
                   )}
                </div>
             )}
-            <div>
-               <Select>
-                  <SelectTrigger className="w-full">
-                     <SelectValue placeholder="Gender" />
-                  </SelectTrigger>
-                  <SelectContent className="bg-black">
-                     <SelectGroup>
-                        <SelectItem className="hover:bg-neutral-700 transition-all duration-250" value="male">Male</SelectItem>
-                        <SelectItem className="hover:bg-neutral-700 transition-all duration-250" value="female">Female</SelectItem>
-                     </SelectGroup>
-                  </SelectContent>
-               </Select>
+
+            <div id="GENDER">
+               <Controller
+                  name="gender"
+                  control={control}
+                  render={({ field }) => (
+                     <Select onValueChange={field.onChange} defaultValue={field.value}>
+                        <SelectTrigger className="w-full bg-black">
+                           <SelectValue placeholder="Select Gender" />
+                        </SelectTrigger>
+                        <SelectContent className="bg-black">
+                           <SelectGroup>
+                              <SelectItem
+                                 className="hover:bg-neutral-700 transition-all duration-250"
+                                 value="MALE">
+                                 Male
+                              </SelectItem>
+                              <SelectItem
+                                 className="hover:bg-neutral-700 transition-all duration-250"
+                                 value="FEMALE">
+                                 Female
+                              </SelectItem>
+                           </SelectGroup>
+                        </SelectContent>
+                     </Select>
+                  )}
+               />
+               {formState.errors.gender && (
+                  <p className=" bg-red-800/20 text-xs text-neutral-300 p-1.5 rounded-sm">
+                     {formState.errors.gender.message}
+                  </p>
+               )}
             </div>
+
             <div className="border-t-2 border-neutral-700 pt-2 mt-2">
                <Button type="submit" className="w-full">
                   Submit
