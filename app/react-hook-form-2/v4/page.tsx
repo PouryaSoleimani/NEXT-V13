a202daf4d31b31c7c0c22551bed3576739599f2d
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-16 20:45:10 +0330

    REACT__HOOK__FORM + ZOD ==> USEFIELDARRAY

--- app/react-hook-form-2/v4/page.tsx
+++ app/react-hook-form-2/v4/page.tsx
@@ -18,45 +18,64 @@ const FormSchema = z.object({
 type FormTypes = z.infer<typeof FormSchema>;
 
 const ReactHookFormV4 = () => {
-   const { control, register, handleSubmit, formState } = useForm<FormTypes>({
+   const { control, register, handleSubmit, formState, reset } = useForm<FormTypes>({
       resolver: zodResolver(FormSchema),
       defaultValues: {
-         items: [{ title: '', price: 0 }],
+         items: [{ title: "", price: 0 }],
       },
    });
 
    //^ USEFIELD ARRAY
    const { fields, append, remove } = useFieldArray({
       control: control,
-      name: 'items'
-   })
+      name: "items",
+   });
+
+   function submitHandler(data : FormTypes){
+      console.log("DATA => ", data);
+      reset
+   }
+
+   console.log(formState.errors)
+   
    return (
       <section className="w-screen h-screen bg-black center flex-col gap-3">
          <h2 className="border-b-2 pb-1 border-sky-900">LENGTH : {fields.length} Items </h2>
-         {fields.map((field, index) => (
-            <div
-               key={field.id}
-               className="flex items-center border border-zinc-800 bg-zinc-900 p-3 rounded-lg">
-               <input
-                  {...register(`items.${index}.title`)}
-                  type="text"
-                  placeholder="title"
-                  className="border bg-black p-2.5 border-zinc-800 rounded-lg m-2 "
-               />
-               <input
-                  type="number"
-                  placeholder="price"
-                  {...register(`items.${index}.price`, { valueAsNumber: true })}
-                  className="border bg-black p-2.5 border-zinc-800 rounded-lg m-1 ml-0 mr-2 "
-               />
-               <Button variant={"destructive"} className="size-10" onClick={() => remove(index)}>
-                  <Trash className="size-6" />
+         <form onSubmit={handleSubmit(submitHandler)}>
+            {fields.map((field, index) => (
+               <div
+                  key={field.id}
+                  className="flex items-center border border-zinc-800 bg-zinc-900 p-3 rounded-lg">
+                  <input
+                     {...register(`items.${index}.title`)}
+                     type="text"
+                     placeholder="title"
+                     className="border bg-black p-2.5 border-zinc-800 rounded-lg m-2 "
+                  />
+                  <input
+                     type="number"
+                     placeholder="price"
+                     {...register(`items.${index}.price`, { valueAsNumber: true })}
+                     className="border bg-black p-2.5 border-zinc-800 rounded-lg m-1 ml-0 mr-2 "
+                  />
+                  <Button variant={"destructive"} className="size-10.5" onClick={() => remove(index)}>
+                     <Trash className="size-5" />
+                  </Button>
+               </div>
+            ))}
+            <div className="flex items-center1 justify-center gap-5 p-5 border-t-2 mt-5 border-zinc-800">
+               <Button
+                  type="button"
+                  variant={"blue"}
+                  className="w-50"
+                  onClick={() => append({ title: "NEW", price: 20 })}>
+                  ADD
+               </Button>
+               <Button variant={"success"} type="submit" className="w-50 text-white">
+                  Submit
                </Button>
             </div>
-         ))}
-         <Button variant={"blue"} className="w-50 mt-3" onClick={() => append({ title: "NEW", price: 20 })}>
-            ADD
-         </Button>
+         </form>
       </section>
    );
 };
