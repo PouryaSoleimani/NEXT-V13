5e2819448197634331664f6b9236586a058aeb34
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-19 16:14:02 +0330

    RHF'

--- app/react-hook-form-2/v5/page.tsx
+++ app/react-hook-form-2/v5/page.tsx
@@ -1,37 +1,24 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import z from "zod";
-
+import React from 'react'
+import z from 'zod';
 
 const FORMSCHEMA = z.object({
    skills: z.array(
       z.object({
-         title: z.string().min(1, "Skill title required"),
-         level: z.number().min(1).max(5),
-         experience: z
-            .array(
-               z.object({
-                  company: z.string().min(1, "Company is Required"),
-                  years: z.number().min(1, "Years is Required"),
-               })
-            )
-            .min(1, "At least 1 Company is Required"),
+         title: z.string(),
+         level: z.number(),
+         experiences: z.array(
+            z.object({
+               company: z.string(),
+               years: z.number(),
+            })
+         ),
       })
    ),
 });
-type FormTypes = z.infer<typeof FORMSCHEMA>;
+const ReactHookFormV5 = () => {
+   return <div className="section">
 
-
-
-const NestedFieldArray = () => {
-   const { control, register, handleSubmit } = useForm<FormTypes>({
-      resolver: zodResolver(FORMSCHEMA),
-      defaultValues: {
-         skills: [{ title: "", level: 1, experience: [{ company: "", years: 1 }] }],
-      },
-   });
-
-   return <div>NestedFieldArray</div>;
+   </div>;
 };
 
-export default NestedFieldArray;
+export default ReactHookFormV5;
