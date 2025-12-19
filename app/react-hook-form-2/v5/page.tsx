27fa23c7e9132cf91fb2ec542dea5c1a0c101976
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-19 13:58:48 +0330

    RHF

--- app/react-hook-form-2/v5/page.tsx
+++ app/react-hook-form-2/v5/page.tsx
@@ -1,48 +1,30 @@
-'use client'
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useFieldArray, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import z from "zod";
-//^ FORM SCHEMA
+
+
 const FORMSCHEMA = z.object({
    skills: z.array(
-         z.object({
-            title: z.string().min(1),
-            level: z.number().min(1).max(5),
-            experiences: z.array(
-                  z.object({
-                     company: z.string().min(1),
-                     years: z.number().min(1),
-                  })
-            )
-               .min(1),
-         })
-   )                                                           
-      .min(1),
+      z.object({
+         title: z.string(),
+         level: z.number(),
+         experience: z.array(z.object({ company: z.string(), years: z.number() })),
+      })
+   ),
 });
+type FormTypes = z.infer<typeof FORMSCHEMA>;
 
-type FormTypes = z.infer<typeof FORMSCHEMA>
 
-const NestedFilledArray = () => {
-   const { control } = useForm<FormTypes>({
+
+const NestedFieldArray = () => {
+   const { control, register, handleSubmit } = useForm<FormTypes>({
       resolver: zodResolver(FORMSCHEMA),
       defaultValues: {
-         skills : [
-            { title: '', level: 1, experiences: [{ company: '', years: 1 }] }
-         ]
-      }
-   })
+         skills: [{ title: "", level: 1, experience: [{ company: "", years: 1 }] }],
+      },
+   });
 
-   const { fields: skillFields, append: addSkill, remove: removeSkill } = useFieldArray({
-   control : control,
-      name: 'skills'
-})
-
-
-   return (
-      <div className="section bg-neutral-950">
-         <form>FORM</form>
-      </div>
-   );
+   return <div>NestedFieldArray</div>;
 };
 
-export default NestedFilledArray;
+export default NestedFieldArray;
