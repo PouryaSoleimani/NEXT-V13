030b7ef7c9b26e16d0a95df9aecd77e77d498040
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-19 18:56:35 +0330

    RHF

--- app/react-hook-form-2/v5/page.tsx
+++ app/react-hook-form-2/v5/page.tsx
@@ -1,4 +1,7 @@
 
+"use client";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useFieldArray, useForm } from 'react-hook-form';
 import z from 'zod';
 
 const FORMSCHEMA = z.object({
@@ -15,8 +18,79 @@ const FORMSCHEMA = z.object({
       })
    ),
 });
+
+type FormTypes = z.infer<typeof FORMSCHEMA>
+// COMPONENT ________________________________________________________________________________
 const ReactHookFormV5 = () => {
-   return <div className="section"></div>;
+   const { control, register } = useForm<FormTypes>({
+      resolver: zodResolver(FORMSCHEMA),
+      defaultValues: {
+         skills: [{ title: "", level: 1, experiences: [{ company: "", years: 0 }] }],
+      },
+   });
+   const {
+      fields: skillFields,
+      append: skillAppend,
+      remove: removeSkill,
+   } = useFieldArray({
+      control: control,
+      name: "skills",
+   });
+
+   return (
+      <div className="section">
+         {skillFields?.map((skill, skillIndex) => {
+            const {
+               fields: expFields,
+               append: addExp,
+               remove: removeExp,
+            } = useFieldArray({
+               control,
+               name: `skills.${skillIndex}.experiences`,
+            });
+
+            return (
+               <div key={skill.id} className="border bg-zinc-800 p-5 rounded-lg">
+                  <input
+                     className="border border-zinc-800 p-2 rounded-lg bg-zinc-900"
+                     {...register(`skills.${skillIndex}.title`)}
+                     placeholder="title"
+                  />
+                  <input
+                     className="border border-zinc-800 p-2 rounded-lg bg-zinc-900"
+                     type="number"
+                     {...register(`skills.${skillIndex}.level`, { valueAsNumber: true })}
+                     placeholder="level"
+                  />
+
+                  {expFields.map((exp, expIndex) => (
+                     <div key={exp.id} className="rounded-lg my-2">
+                        <input
+                           className="border border-zinc-800 p-2 rounded-lg bg-zinc-900"
+                           {...register(`skills.${skillIndex}.experiences.${expIndex}.company`)}
+                        />
+                        <input
+                           className="border border-zinc-800 p-2 rounded-lg bg-zinc-900"
+                           type="number"
+                           {...register(`skills.${skillIndex}.experiences.${expIndex}.years`, { valueAsNumber: true })}
+                        />
+                        <button className="btn" type="button" onClick={() => removeExp(expIndex)}>
+                           Remove Experience
+                        </button>
+                     </div>
+                  ))}
+
+                  <button className="btn" type="button" onClick={() => addExp({ company: "", years: 1 })}>
+                     Add Experience
+                  </button>
+                  <button className="btn" type="button" onClick={() => removeSkill(skillIndex)}>
+                     Remove Skill
+                  </button>
+               </div>
+            );
+         })}
+      </div>
+   );
 };
 
 export default ReactHookFormV5;
