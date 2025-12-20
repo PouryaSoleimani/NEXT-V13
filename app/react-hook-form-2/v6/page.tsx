7bc25cc2217b323d1462ba05e00e6164b82de004
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-20 21:46:38 +0330

    RHF

--- app/react-hook-form-2/v6/page.tsx
+++ app/react-hook-form-2/v6/page.tsx
@@ -1,24 +1,24 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import z from "zod";
 
-const FORMSCHEMA = z.object({
+export const FORMSCHEMAS = z.object({
    skills: z
       .array(
          z.object({
             title: z.string(),
-            level: z.coerce
+            level: z
                .number({ error: "Level Must be a number" })
                .min(1, "Minimun 1")
-               .max(5, "Maximum 5")
-               .int(),
+               .max(5, "Maximum 5"),
             experiences: z
                .array(
                   z.object({
                      company: z.string().min(1, "Company Name is Required"),
-                     years: z.coerce
+                     years: z
                         .number({ error: "Years Must Be a number" })
                         .min(1, "Minimun 1")
                         .max(5, "Maximum 5")
-                        .int()
                         .nullable(),
                   })
                )
@@ -27,8 +27,12 @@ const FORMSCHEMA = z.object({
       )
       .min(1, "At least 1 Skill is Required"),
 });
+type FormTypes = z.infer<typeof FORMSCHEMAS>
 
 const ReactHookFormV6 = () => {
+  const { control } = useForm<FormTypes>({
+     resolver: zodResolver(FORMSCHEMAS),
+  });
    return <div>ReactHookFormV6</div>;
 };
 
