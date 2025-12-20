e29686276b314658338b032290343f753b47c9f9
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-20 18:59:05 +0330

    RHF

--- app/react-hook-form-2/v5/page.tsx
+++ app/react-hook-form-2/v5/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useFieldArray, useForm } from "react-hook-form";
 import z from "zod";
@@ -10,14 +10,16 @@ import { cn } from "@/lib/utils";
 const FORMSCHEMA = z.object({
    skills: z.array(
       z.object({
-         title: z.string().min(1, 'Title is Required'),
-         level: z.number().min(1, 'Years Is Required').nullable(),
-         experiences: z.array(
-            z.object({
-               company: z.string().min(1, 'Company is Required'),
-               years: z.number().nullable(),
-            })
-         ).min(1, 'At Least 1 Experience Field is Required'),
+         title: z.string().min(1, "Title is Required"),
+         level: z.number().min(1, "Years Is Required").nullable(),
+         experiences: z
+            .array(
+               z.object({
+                  company: z.string().min(1, "Company is Required"),
+                  years: z.number().nullable(),
+               })
+            )
+            .min(1, "At Least 1 Experience Field is Required"),
       })
    ),
 });
@@ -39,19 +41,21 @@ const ReactHookFormV5 = () => {
       },
    });
 
-   const { fields: SkillFields, append: SkillAppend, remove: SkillRemove, } = useFieldArray({
+   const {
+      fields: SkillFields,
+      append: SkillAppend,
+      remove: SkillRemove,
+   } = useFieldArray({
       control: control,
       name: "skills",
    });
 
    function SubmitHandler(data: FormTypes) {
       console.info("DATA => ", data);
-      toast.success('FORM SUBMITTED', { position: 'top-center' })
+      toast.success("FORM SUBMITTED", { position: "top-center" });
       reset({
-         skills: [
-            { title: "", level: null, experiences: [{ company: "", years: null }], },
-         ],
-      })
+         skills: [{ title: "", level: null, experiences: [{ company: "", years: null }] }],
+      });
    }
 
    return (
@@ -61,7 +65,10 @@ const ReactHookFormV5 = () => {
                <div className="flex flex-col items-center gap-3 bg-zinc-800 p-3 rounded-lg shadow-inner shadow-white/30 text-rose-900 w-86">
                   <TriangleAlert />
                   <h3>NO SKILLS TO SHOW</h3>
-                  <button type="button" onClick={() => SkillAppend({ title: '', level: 1, experiences: [{ company: '', years: 0 }] })} className="btn flex items-center-safe gap-3">
+                  <button
+                     type="button"
+                     onClick={() => SkillAppend({ title: "", level: 1, experiences: [{ company: "", years: 0 }] })}
+                     className="btn flex items-center-safe gap-3">
                      <PlusCircle className="size-4" />
                      ADD FIRST SKILL
                   </button>
@@ -79,19 +86,17 @@ const ReactHookFormV5 = () => {
                />
             ))}
 
-            <div id="BUTTONS" className={cn("flex gap-3 w-[50%] mx-auto  py-5", SkillFields.length === 0 && 'w-full')}>
+            <div id="BUTTONS" className={cn("flex gap-3 w-[50%] mx-auto  py-5", SkillFields.length === 0 && "w-full")}>
                <button className=" basis-1/2 text-sm p-3 rounded-lg border border-emerald-950 shadow-inner shadow-white/10  mx-auto bg-emerald-900 hover:bg-emerald-800 transition-all duration-300 ">
                   SUBMIT FORM
                </button>
                <button
-                  onClick={() => SkillAppend({ title: '', level: 1, experiences: [{ company: '', years: 0 }] })}
+                  onClick={() => SkillAppend({ title: "", level: 1, experiences: [{ company: "", years: 0 }] })}
                   type="button"
-                  className="basis-1/2 text-sm p-3 rounded-lg border border-sky-950 shadow-inner shadow-white/10  mx-auto bg-sky-900 hover:bg-sky-800 transition-all duration-300 "
-               >
+                  className="basis-1/2 text-sm p-3 rounded-lg border border-sky-950 shadow-inner shadow-white/10  mx-auto bg-sky-900 hover:bg-sky-800 transition-all duration-300 ">
                   ADD SKILL
                </button>
             </div>
-
          </form>
       </div>
    );
