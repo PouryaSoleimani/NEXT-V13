176a70523021b771d85660555de5fdd10e030d95
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-20 12:10:27 +0330

    RHF

--- pwa-settings-2.js
+++ pwa-settings-2.js
@@ -9,99 +9,99 @@ const path = require("path");
 const isDev = process.env.NODE_ENV !== "production";
 
 const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  runtimeCaching,
-  buildExcludes: ["app-build-manifest.json"],
-  reloadOnOnline: true,
-  disable: process.env.NODE_ENV === "development",
+   dest: "public",
+   register: true,
+   skipWaiting: true,
+   runtimeCaching,
+   buildExcludes: ["app-build-manifest.json"],
+   reloadOnOnline: true,
+   disable: process.env.NODE_ENV === "development",
 });
 
 const generateAppDirEntry = (entry) => {
-  const packagePath = require.resolve("next-pwa");
-  const packageDirectory = path.dirname(packagePath);
-  const registerJs = path.join(packageDirectory, "register.js");
+   const packagePath = require.resolve("next-pwa");
+   const packageDirectory = path.dirname(packagePath);
+   const registerJs = path.join(packageDirectory, "register.js");
 
-  return entry().then((entries) => {
-    if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
-      if (Array.isArray(entries["main-app"])) {
-        entries["main-app"].unshift(registerJs);
-      } else if (typeof entries["main-app"] === "string") {
-        entries["main-app"] = [registerJs, entries["main-app"]];
+   return entry().then((entries) => {
+      if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
+         if (Array.isArray(entries["main-app"])) {
+            entries["main-app"].unshift(registerJs);
+         } else if (typeof entries["main-app"] === "string") {
+            entries["main-app"] = [registerJs, entries["main-app"]];
+         }
       }
-    }
 
-    return entries;
-  });
+      return entries;
+   });
 };
 
 const nextConfig = {
-  reactStrictMode: false,
+   reactStrictMode: false,
 
-  webpack(config) {
-    if (!isDev) {
-      const entry = generateAppDirEntry(config.entry);
-      config.entry = () => entry;
-    }
-
-    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.(".inline.svg"));
-
-    config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
-      {
-        ...fileLoaderRule,
-        test: /\.inline.svg$/i,
-        resourceQuery: /url/, // *.inline.svg?url
-      },
-      // Convert all other *.inline.svg imports to React components
-      {
-        test: /\.inline.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: {
-          not: [...fileLoaderRule.resourceQuery.not, /url/],
-        }, // exclude if *.inline.svg?url
-        use: ["@svgr/webpack"],
+   webpack(config) {
+      if (!isDev) {
+         const entry = generateAppDirEntry(config.entry);
+         config.entry = () => entry;
       }
-    );
 
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.inline.svg$/i;
+      const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.(".inline.svg"));
 
-    return config;
-  },
-  env: {
-    NEXT_PUBLIC_CORE_FILES_URL: process.env.NEXT_PUBLIC_CORE_FILES_URL,
-    NEXT_PUBLIC_FILES_URL: process.env.NEXT_PUBLIC_FILES_URL,
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-    NEXT_PUBLIC_PY_URL: process.env.NEXT_PUBLIC_PY_URL,
-    NEXT_PUBLIC_SESSION_DOMAIN: process.env.NEXT_PUBLIC_SESSION_DOMAIN,
-    NEXT_PUBLIC_SESSION_COOKIE: process.env.NEXT_PUBLIC_SESSION_COOKIE,
-    NEXT_PUBLIC_VERSION: process.env.NEXT_PUBLIC_VERSION,
-    NEXT_PUBLIC_LANDING_MODE: process.env.NEXT_PUBLIC_LANDING_MODE,
-    NEXT_PRIVATE_BUILD_WORKER: "wasm",
-  },
-  experimental: {
-    // reactCompiler: { compilationMode: 'annotation', },
-    optimizePackageImports: ["@react-pdf/renderer", "@react-pdf", "react-icons", "xlsx"],
-    // forceSwcTransforms: true,
-  },
-  // turbopack: {
-  // 	rules: {
-  // 		'*.inline.svg': {
-  // 			loaders: ['@svgr/webpack'],
-  // 			as: '*.js',
-  // 		},
-  // 	},
-  // },
-  images: {
-    unoptimized: false,
-    remotePatterns: [
-      { protocol: "http", hostname: "**" },
-      { protocol: "https", hostname: "**" },
-    ],
-  },
+      config.module.rules.push(
+         // Reapply the existing rule, but only for svg imports ending in ?url
+         {
+            ...fileLoaderRule,
+            test: /\.inline.svg$/i,
+            resourceQuery: /url/, // *.inline.svg?url
+         },
+         // Convert all other *.inline.svg imports to React components
+         {
+            test: /\.inline.svg$/i,
+            issuer: fileLoaderRule.issuer,
+            resourceQuery: {
+               not: [...fileLoaderRule.resourceQuery.not, /url/],
+            }, // exclude if *.inline.svg?url
+            use: ["@svgr/webpack"],
+         }
+      );
+
+      // Modify the file loader rule to ignore *.svg, since we have it handled now.
+      fileLoaderRule.exclude = /\.inline.svg$/i;
+
+      return config;
+   },
+   env: {
+      NEXT_PUBLIC_CORE_FILES_URL: process.env.NEXT_PUBLIC_CORE_FILES_URL,
+      NEXT_PUBLIC_FILES_URL: process.env.NEXT_PUBLIC_FILES_URL,
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+      NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+      NEXT_PUBLIC_PY_URL: process.env.NEXT_PUBLIC_PY_URL,
+      NEXT_PUBLIC_SESSION_DOMAIN: process.env.NEXT_PUBLIC_SESSION_DOMAIN,
+      NEXT_PUBLIC_SESSION_COOKIE: process.env.NEXT_PUBLIC_SESSION_COOKIE,
+      NEXT_PUBLIC_VERSION: process.env.NEXT_PUBLIC_VERSION,
+      NEXT_PUBLIC_LANDING_MODE: process.env.NEXT_PUBLIC_LANDING_MODE,
+      NEXT_PRIVATE_BUILD_WORKER: "wasm",
+   },
+   experimental: {
+      // reactCompiler: { compilationMode: 'annotation', },
+      optimizePackageImports: ["@react-pdf/renderer", "@react-pdf", "react-icons", "xlsx"],
+      // forceSwcTransforms: true,
+   },
+   // turbopack: {
+   // 	rules: {
+   // 		'*.inline.svg': {
+   // 			loaders: ['@svgr/webpack'],
+   // 			as: '*.js',
+   // 		},
+   // 	},
+   // },
+   images: {
+      unoptimized: false,
+      remotePatterns: [
+         { protocol: "http", hostname: "**" },
+         { protocol: "https", hostname: "**" },
+      ],
+   },
 };
 
 module.exports = withPWA(nextConfig);
--- pwa-settings.js
+++ pwa-settings.js
@@ -1,41 +1,41 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-  buildExcludes: [/middleware-manifest\.json$/],
-  fallbacks: { image: "/offline.png", document: "/offline.html" },
-  cacheStartUrl: false,
-  runtimeCaching: [
-    {
-      urlPattern: /\/api\/todoitem/,
-      handler: "StaleWhileRevalidate",
-      method: "GET",
-      options: {
-        cacheName: "todoApp-api",
-        expiration: { maxEntries: 64, maxAgeSeconds: 24 * 60 * 60 },
+   dest: "public",
+   register: true,
+   skipWaiting: true,
+   disable: process.env.NODE_ENV === "development",
+   buildExcludes: [/middleware-manifest\.json$/],
+   fallbacks: { image: "/offline.png", document: "/offline.html" },
+   cacheStartUrl: false,
+   runtimeCaching: [
+      {
+         urlPattern: /\/api\/todoitem/,
+         handler: "StaleWhileRevalidate",
+         method: "GET",
+         options: {
+            cacheName: "todoApp-api",
+            expiration: { maxEntries: 64, maxAgeSeconds: 24 * 60 * 60 },
+         },
       },
-    },
-    {
-      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
-      handler: "CacheFirst",
-      options: {
-        cacheName: "google-fonts",
-        expiration: { maxEntries: 4, maxAgeSeconds: 365 * 24 * 60 * 60 },
+      {
+         urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+         handler: "CacheFirst",
+         options: {
+            cacheName: "google-fonts",
+            expiration: { maxEntries: 4, maxAgeSeconds: 365 * 24 * 60 * 60 },
+         },
       },
-    },
-  ],
+   ],
 });
 
 module.exports = withPWA({
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "stage.sizhelp.ir", pathname: "/**" },
-      { protocol: "https", hostname: "siz.sizhelp.ir", pathname: "/**" },
-      { protocol: "https", hostname: "siz.4siz.ir", pathname: "/**" },
-      { protocol: "https", hostname: "core.4siz.ir", pathname: "/**" },
-    ],
-  },
-  env: { NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL },
+   images: {
+      remotePatterns: [
+         { protocol: "https", hostname: "stage.sizhelp.ir", pathname: "/**" },
+         { protocol: "https", hostname: "siz.sizhelp.ir", pathname: "/**" },
+         { protocol: "https", hostname: "siz.4siz.ir", pathname: "/**" },
+         { protocol: "https", hostname: "core.4siz.ir", pathname: "/**" },
+      ],
+   },
+   env: { NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL },
 });
