56e1d5faf54f41f93e5ae7d206bf32516540e759
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-21 21:36:48 +0330

    RHF

--- app/react-hook-form-2/v6/page.tsx
+++ app/react-hook-form-2/v6/page.tsx
@@ -1,44 +1,41 @@
-"use client";
+'use client'
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Resolver, useForm } from "react-hook-form";
 import z from "zod";
 
-export const FORMSCHEMAS = z.object({
+export const FORMSCHEMA = z.object({
    skills: z
       .array(
          z.object({
-            title: z.string(),
+            title: z.string().min(1, "Title is Required"),
             level: z.coerce
-               .number({ error: "Level Must be a number" })
-               .min(1, "Minimun 1")
-               .max(5, "Maximum 5"),
+               .number()
+               .min(1, "level Must be >= 1")
+               .max(5, "Level Must Be between 1 to 5")
+               .int(),
             experiences: z
                .array(
                   z.object({
-                     company: z.string().min(1, "Company Name is Required"),
-                     years: z.coerce
-                        .number({ error: "Years Must Be a number" })
-                        .min(1, "Minimun 1")
-                        .max(5, "Maximum 5")
-                        .nullable(),
+                     company: z.string().min(1, "Company is Required"),
+                     years: z.coerce.number().min(1, "Years is Required").int(),
                   })
                )
-               .min(1, "At least 1 Company is Required"),
+               .min(1, "At least 1 Experience is Required"),
          })
       )
       .min(1, "At least 1 Skill is Required"),
 });
-type FormTypes = z.infer<typeof FORMSCHEMAS>;
+
+type FormTypes = z.infer<typeof FORMSCHEMA>;
 
 const ReactHookFormV6 = () => {
    const { control } = useForm<FormTypes>({
-      resolver: zodResolver(FORMSCHEMAS) as Resolver<FormTypes>,
+      resolver: zodResolver(FORMSCHEMA) as Resolver<FormTypes>,
       defaultValues: {
-         skills: [{ title: "", level: 0, experiences: [{ company: "", years: 0 }] }],
+         skills: [{ title: "", level: 1, experiences: [{ company: "", years: 1 }] }],
       },
-      mode: "onBlur",
    });
-   return <div className="section bg-black"></div>;
+   return <div>ReactHookFormV6</div>;
 };
 
 export default ReactHookFormV6;
