91a2792c52f9ec57db317da87cc102cd3334f4e3
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-22 10:43:03 +0330

    RHF'

--- app/react-hook-form-2/v5/page.tsx
+++ app/react-hook-form-2/v5/page.tsx
@@ -31,14 +31,14 @@ export type FormTypes = z.infer<typeof FORMSCHEMA>;
 const ReactHookFormV5 = () => {
    // USE FORM
    const {
-      control,
-      register,
-      handleSubmit,
-      formState: { errors },
-      reset,
+       control,
+       register,
+       handleSubmit,
+       formState: { errors },
+       reset,
    } = useForm<FormTypes>({
-      resolver: zodResolver(FORMSCHEMA),
-      defaultValues: { skills: [{ title: "", level: null, experiences: [{ company: "", years: null }] }] },
+       resolver: zodResolver(FORMSCHEMA),
+       defaultValues: { skills: [{ title: "", level: null, experiences: [{ company: "", years: null }] }] },
    });
 
    const {
