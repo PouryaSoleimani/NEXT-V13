703861610df75d0d006214b4e542517dac53f797
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-28 11:53:38 +0330

    RHF

--- app/react-hook-form-2/v7/page.tsx
+++ app/react-hook-form-2/v7/page.tsx
@@ -2,7 +2,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Controller, FormProvider, useForm, useWatch } from "react-hook-form";
+import { Controller, FormProvider, Resolver, useForm, useWatch } from "react-hook-form";
 import z from "zod";
 import FieldError from "./_components/FieldError";
 import toast from "react-hot-toast";
@@ -13,18 +13,21 @@ const FORMSCHEMAV7 = z.object({
       .string()
       .min(1, "Title is Required")
       .max(5, "Maximum 5 Characters are allowed"),
+   price: z.coerce.number("Price must be a Number").min(1, 'Price Must be >= 1').nullable()
 });
 
 export type FormTypesv7 = z.infer<typeof FORMSCHEMAV7>;
 
 const ReactHookFormV7 = () => {
    const methods = useForm<FormTypesv7>({
-      resolver: zodResolver(FORMSCHEMAV7),
+      resolver: zodResolver(FORMSCHEMAV7) as Resolver<FormTypesv7>,
+      mode:'onBlur',
       defaultValues: {
          title: "",
+         price: null
       },
    });
-   
+
    function submitHandler(data: FormTypesv7) {
       console.info("V7 DATA =>", data);
       toast.success("FORM SUBMITTED", { position: "top-center" });
@@ -35,17 +38,22 @@ const ReactHookFormV7 = () => {
       control: methods.control,
    });
 
+   const priceValue = useWatch({
+      control: methods.control,
+      name: 'price'
+   })
+
    useEffect(() => {
       if (titleValue !== "") {
          console.info("RERENDER");
       }
-   return;
-   }, [titleValue])
+      return;
+   }, [titleValue ,priceValue])
    
    return (
       <div className="section bg-zinc-900">
          <FormProvider {...methods}>
-            <form onSubmit={methods.handleSubmit(submitHandler)}>
+            <form onSubmit={methods.handleSubmit(submitHandler)} className="flex flex-col gap-2">
                <Controller
                   name="title"
                   control={methods.control}
@@ -60,9 +68,24 @@ const ReactHookFormV7 = () => {
                      </div>
                   )}
                />
+               <Controller 
+                  name="price"
+                  control={methods.control}
+                  render={({field}) => (
+                     <div className="flex flex-col gap-1">
+                        <Input 
+                           value={field.value ?? ''}
+                           onChange={field.onChange}
+                           placeholder={'$ Price'}
+                        />
+                        <FieldError name={field.name} />
+                     </div>
+                  )}
+                     />F
+               <hr className="text-zinc-700" />
                <Button
                   type="submit"
-                  className="my-2 bg-emerald-900 hover:bg-emerald-800 w-full">
+                  className="my-1 bg-emerald-900 hover:bg-emerald-800 w-full">
                   Submit
                </Button>
             </form>
