cc91b31b94bc703a6d7586a789489a687bdd1178
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-29 23:08:26 +0330

    RHF => USEFIELDARRAY

--- app/react-hook-form-2/v8/page.tsx
+++ app/react-hook-form-2/v8/page.tsx
@@ -6,46 +6,58 @@ import z from "zod"
 import ErrorFieldV8 from "./_components/ErrorFieldV8"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
-export const FORMSCHEMAV8 = z.object({
-  title: z.string().min(1, 'Title is Required')
-})
+import { useState } from "react"
 
-export type FormTypesV8 = z.infer<typeof FORMSCHEMAV8>
+export const StepSchema1 = z.object({ email: z.email("Email is Not Valid") });
+
+export type FormTypesV8 = z.infer<typeof StepSchema1>;
 
 const ReactHookFormV8 = () => {
+  const [step, setStep] = useState<1 | 2 | 3>(1);
+
   const methods = useForm<FormTypesV8>({
     resolver: zodResolver(FORMSCHEMAV8)
   })
+  
+  const { isValid, isSubmitting } = methods.formState
 
   function sumbitHandler(data: FormTypesV8) {
-    console.log('DATA => ', data)
+    console.log("DATA => ", data);
   }
 
   return (
     <div className="bg-slate-950 screen center">
       <FormProvider {...methods}>
-        <form onSubmit={methods.handleSubmit(sumbitHandler)} className="border border-slate-800 rounded-lg p-4 bg-black">
+        <form
+          onSubmit={methods.handleSubmit(sumbitHandler)}
+          className="border border-slate-800 rounded-lg p-4 bg-black">
           <Controller
             control={methods.control}
             name="title"
             render={({ field }) => (
-              <div className="flex flex-col gap-2">
-                <Label>{field.name.toUpperCase()}</Label>
-              <Input
-                value={field.value}
-                onChange={field.onChange}
-                aria-invalid={!!methods.formState.errors.title}
-                placeholder={field.name.toUpperCase()}
+              <div className="flex flex-col gap-1">
+                <Label className="mb-0.5">{field.name.toUpperCase()}</Label>
+                <Input
+                  value={field.value}
+                  onChange={field.onChange}
+                  aria-invalid={!!methods.formState.errors.email}
+                  placeholder={field.name.toUpperCase()}
                 />
                 <ErrorFieldV8 name={field.name} />
               </div>
             )}
           />
-          <Button type="submit" className="mx-auto my-3 w-full bg-emerald-950 hover:bg-emerald-800">SUBMIT</Button>
+          <Button
+            disabled={!isValid || isSubmitting}
+            aria-disabled={!isValid || isSubmitting}
+            type="submit"
+            className="mx-auto my-3 w-full bg-emerald-950 hover:bg-emerald-800">
+            SUBMIT
+          </Button>
         </form>
       </FormProvider>
     </div>
-  )
+  );
 }
 
 export default ReactHookFormV8
