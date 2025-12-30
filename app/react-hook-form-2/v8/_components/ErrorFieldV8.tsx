a92a95c09902aef323341831a7fa653054c38d5b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-30 18:11:42 +0330

    RHF

--- app/react-hook-form-2/v8/_components/ErrorFieldV8.tsx
+++ app/react-hook-form-2/v8/_components/ErrorFieldV8.tsx
@@ -2,8 +2,8 @@ import { get, Path, useFormState } from 'react-hook-form'
 import { FormTypesV8 } from '../page'
 
 const ErrorFieldV8 = ({name} : {name : Path<FormTypesV8>}) => {
-  const { errors } = useFormState()
-  const error = get(errors, name)
+  const { errors } = useFormState();
+  const error = get(errors, name);
 
   if (!error) return null;
 
