0c2c6aaf8fd67e7496f3938c9abeb07978ddd849
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-12-31 11:05:04 +0330

    RHF

--- app/react-hook-form-2/v8/page.tsx
+++ app/react-hook-form-2/v8/page.tsx
@@ -61,19 +61,12 @@ export type FormTypesV8 =
 
 const ReactHookFormV8 = () => {
   const [step, setStep] = useState<1 | 2 | 3>(1);
-  const [inputType, setInputType] = useState<
-    "password" | "text"
-  >("password");
+  const [inputType, setInputType] = useState< "password" | "text" >("password");
 
   const methods = useForm<any>({
     mode: "onChange",
     resolver: zodResolver(schemaByStep[step]),
-    defaultValues: {
-      email: "",
-      password: "",
-      skills: [],
-      acceptTerms: false,
-    },
+    defaultValues: { email: "", password: "", skills: [], acceptTerms: false },
   });
 
   const { isValid, isSubmitting } = methods.formState;
@@ -115,23 +108,17 @@ const ReactHookFormV8 = () => {
           {/* STEP 1 */}
           {step == 1 && (
             <>
-              <h2 className='border-b-2 border-stone-700'>
-                STEP 1
-              </h2>
+              <h2 className='border-b-2 border-stone-700'>STEP 1</h2>
               <Controller
                 control={methods.control}
                 name='email'
                 render={({ field }) => (
                   <div className='flex flex-col gap-1'>
-                    <Label className='mb-0.5'>
-                      {field.name.toUpperCase()}
-                    </Label>
+                    <Label className='mb-0.5'>{field.name.toUpperCase()}</Label>
                     <Input
                       value={field.value}
                       onChange={field.onChange}
-                      aria-invalid={
-                        !!methods.formState.errors.email
-                      }
+                      aria-invalid={!!methods.formState.errors.email}
                       placeholder={field.name.toUpperCase()}
                     />
                     <ErrorFieldV8 name={field.name} />
@@ -143,24 +130,19 @@ const ReactHookFormV8 = () => {
                 name='password'
                 render={({ field }) => (
                   <div className='flex flex-col gap-1 relative inset-0'>
-                    <Label className='mb-0.5'>
-                      {field.name.toUpperCase()}
-                    </Label>
+                    <Label className='mb-0.5'>{field.name.toUpperCase()}</Label>
                     <Input
                       type={inputType}
                       value={field.value}
                       onChange={field.onChange}
-                      aria-invalid={
-                        !!methods.formState.errors.password
-                      }
+                      aria-invalid={!!methods.formState.errors.password}
                       placeholder={field.name.toUpperCase()}
                     />
                     <button
                       type='button'
                       className={cn(
                         "absolute cursor-pointer group right-1.5 top-1/2",
-                        methods.formState.errors.password &&
-                          "top-7"
+                        methods.formState.errors.password && "top-7"
                       )}
                       onClick={() =>
                         inputType == "password"
@@ -183,16 +165,12 @@ const ReactHookFormV8 = () => {
           {/* STEP 2 */}
           {step == 2 && (
             <div>
-              <h2 className='border-b-2 border-stone-700'>
-                STEP 2
-              </h2>
+              <h2 className='border-b-2 border-stone-700'>STEP 2</h2>
               {fields.length == 0 && (
                 <p className='text-stone-500 flex flex-col border-b-2 pb-4 border-stone-700 items-center-safe gap-3 my-3 text-sm'>
                   <PlusCircle className='size-5' />
                   No Skills , Try Add Skills Now{" "}
-                  <span className='animate-bounce text-xl'>
-                    ↓
-                  </span>
+                  <span className='animate-bounce text-xl'>↓</span>
                 </p>
               )}
               {fields?.map((item, index) => (
@@ -207,9 +185,7 @@ const ReactHookFormV8 = () => {
                           onChange={field.onChange}
                           placeholder='Skill Name'
                         />
-                        <ErrorFieldV8
-                          name={`skills[${index}]` as any}
-                        />
+                        <ErrorFieldV8 name={`skills[${index}]` as any} />
                       </div>
                       <div className='flex items-center gap-1 pt-0.5'>
                         <Tooltip>
@@ -234,9 +210,7 @@ const ReactHookFormV8 = () => {
                 <Tooltip>
                   <TooltipTrigger asChild>
                     <Button
-                      onClick={() =>
-                        append({}, { shouldFocus: true })
-                      }
+                      onClick={() => append({}, { shouldFocus: true })}
                       className='size-8 w-1/2'
                       variant={"blue"}>
                       +
@@ -269,19 +243,14 @@ const ReactHookFormV8 = () => {
           {/* STEP 3 */}
           {step == 3 && (
             <>
-              <h2 className='border-b-2 border-stone-700'>
-                STEP 3
-              </h2>
+              <h2 className='border-b-2 border-stone-700'>STEP 3</h2>
               <Controller
                 name='acceptTerms'
                 control={methods.control}
                 render={({ field }) => (
                   <div className='flex flex-col gap-1'>
                     <div className='flex flex-row-reverse gap-2'>
-                      <Label>
-                        Accept Terms And Privacy and Policy
-                        Rules
-                      </Label>
+                      <Label>Accept Terms And Privacy and Policy Rules</Label>
                       <Checkbox
                         onCheckedChange={field.onChange}
                         className='accent-cyan-500'
@@ -294,23 +263,13 @@ const ReactHookFormV8 = () => {
                         Please Accept the Terms to continue
                       </p>
                     )}
-                    <ErrorFieldV8
-                      name={field.name as any}
-                    />
+                    <ErrorFieldV8 name={field.name as any} />
                   </div>
                 )}
               />
               <Button
-                disabled={
-                  !isValid ||
-                  isSubmitting ||
-                  hasAccepted == false
-                }
-                aria-disabled={
-                  !isValid ||
-                  isSubmitting ||
-                  hasAccepted == false
-                }
+                disabled={!isValid || isSubmitting || hasAccepted == false}
+                aria-disabled={!isValid || isSubmitting || hasAccepted == false}
                 type='submit'
                 className='mx-auto w-full bg-emerald-950 hover:bg-emerald-800'>
                 SUBMIT
