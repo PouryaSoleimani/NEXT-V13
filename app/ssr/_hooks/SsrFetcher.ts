9aadc41e698830e0db1162efa9037e6e3ba978bc
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-05 15:25:40 +0330

    SSR

--- app/ssr/_hooks/SsrFetcher.ts
+++ app/ssr/_hooks/SsrFetcher.ts
@@ -1,5 +1,5 @@
-import React from 'react'
-
+'use server'
+//& SSR FETCHER HOOK
 export default async function SsrFetcher(url: string) {
   const res = await fetch(url, { cache: "no-store" });
   const data = await res.json();
