b93812524bc704cef5c77bf12ed73857ca6a3b35
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-05 20:47:39 +0330

    SSR

--- app/ssr/page.tsx
+++ app/ssr/page.tsx
@@ -5,9 +5,11 @@ import useServerSideFetcher from "./_hooks/useServerSideFetcher";
 
 const SsrPage = async () => {
   const users = await useServerSideFetcher("https://jsonplaceholder.typicode.com/users", "no-store", 50);
+
   if (!users) {
     return <Loading />;
   }
+  
   return <div>{users ? <ListWrapper list={users} /> : null}</div>;
 };
 
