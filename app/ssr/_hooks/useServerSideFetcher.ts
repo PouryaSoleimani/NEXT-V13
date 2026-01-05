22afbb01d7df9d5cd7d0581969a06ac0f4784557
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-05 15:45:03 +0330

    SSR

--- app/ssr/_hooks/useServerSideFetcher.ts
+++ app/ssr/_hooks/useServerSideFetcher.ts
@@ -3,7 +3,7 @@
 export default async function useServerSideFetcher(url: string) {
   const res = await fetch(url, { cache: "no-store" });
   const data = await res.json();
-  if(!data) {
+  if (!data) {
     throw new Error("Fetch Failed");
   }
   return data;
