6f4f9cf1f3f76900ebe88d8d55779affc162d4cd
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-05 20:46:13 +0330

    SSR

--- app/ssr/_hooks/useClientSideFetcher.ts
+++ app/ssr/_hooks/useClientSideFetcher.ts
@@ -1,15 +1,15 @@
-//& CLIENT SIDE FETCHER HOOK
+//^ CLIENT SIDE FETCHER HOOK
 import axios from "axios";
 import useSWR from "swr";
 
-//& HOOK
+//^ HOOK
 export default function useClientSideFetcher(url: string) {
   const fetcher = () =>
     axios
       .get(url)
       .then((res) => res.data)
       .catch((err) => err.message);
-      
+
   const { data, isLoading, error, mutate } = useSWR(url, fetcher);
 
   return { data, isLoading, error, mutate };
