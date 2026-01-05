9b068307c7e46d4ebabded0e31f2a7538574ce11
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-05 17:52:10 +0330

    SSR

--- app/ssr/_hooks/useServerSideFetcher.ts
+++ app/ssr/_hooks/useServerSideFetcher.ts
@@ -1,7 +1,7 @@
 "use server";
 //^ SSR FETCHER HOOK
 
-export type cache = "no-store" | "default" | "no-cache";
+export type cache = "no-store" | "default" | "no-cache" | "force-cache";
 
 export default async function useServerSideFetcher(url: string, cache: cache, revalidate?: number) {
   const res = await fetch(url, { cache: cache, next: revalidate ? { revalidate: revalidate } : undefined });
