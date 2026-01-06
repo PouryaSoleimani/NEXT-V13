c42bc79bd81ebea73a36a67452f8de09ef2e23c7
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-07 01:04:03 +0330

    SSR

--- app/ssr-3/page.tsx
+++ app/ssr-3/page.tsx
@@ -4,10 +4,7 @@ import TodosWrapper2 from "./_components/TodosWrapper2";
 import useServerSideFetcher from "./_hooks/useServerSideFetcher";
 
 const SsrPage3 = async () => {
-  const todos = await useServerSideFetcher({
-    url: "https://jsonplaceholder.typicode.com/todos",
-    cache: "no-store",
-  });
+const todos = await useServerSideFetcher({ url: "https://jsonplaceholder.typicode.com/todos", cache: "no-store", });
 
   return (
     <div className='center'>
