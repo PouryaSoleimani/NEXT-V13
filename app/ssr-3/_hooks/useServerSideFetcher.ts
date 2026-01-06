7aacdf4d951eec91dd0a58340364c6a53eb5d15b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-06 17:23:38 +0330

    SSR

--- app/ssr-3/_hooks/useServerSideFetcher.ts
+++ app/ssr-3/_hooks/useServerSideFetcher.ts
@@ -5,10 +5,7 @@ interface Props {
 }
 
 const useServerSideFetcher = async ({ url, cache, revalidate }: Props) => {
-  const res = await fetch(url, {
-    cache: cache,
-    next: { revalidate: revalidate ? revalidate : undefined },
-  });
+  const res = await fetch(url, { cache: cache, next: { revalidate: revalidate ? revalidate : undefined } });
   const data = await res.json();
   return data;
 };
