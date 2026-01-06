328665f6471724181e7f92886db28ae6627ca293
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-06 14:52:45 +0330

    SSR

--- app/ssr-2/_components/TodoItem.tsx
+++ app/ssr-2/_components/TodoItem.tsx
@@ -5,7 +5,7 @@ const TodoItem = ({ todoItem }: { todoItem: SingleTodoItem }) => {
   return (
     <div className='flex hover:border-b-white transition-all duration-250 cursor-pointer items-center justify-between gap-2 py-2 border-b-2 border-b-stone-600 text-sm font-semibold bg-stone-900 my-2 rounded-sm px-2 grow'>
       <p>
-        {todoItem.id} - {todoItem.title.slice(0, 30)}{" "}
+        {todoItem.id} - {todoItem.title.slice(0, 30)}
       </p>
       <span className='border-l-2  pl-4 border-stone-700'>{todoItem.completed ? "✔" : "𐄂"} </span>
     </div>
