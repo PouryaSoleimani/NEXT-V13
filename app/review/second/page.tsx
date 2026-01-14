391a054c623abbbfaafdab878fe7422a0e9350ce
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-14 20:48:12 +0330

    FRONT__REVIEW

--- app/review/second/page.tsx
+++ app/review/second/page.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button";
-import { Drawer, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTrigger } from "@/components/ui/drawer";
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
 import { Separator } from "@/components/ui/separator";
-import { Menu } from "lucide-react";
+import { Code, Home, Info, Menu, X } from "lucide-react";
+import Link from "next/link";
 
 const SecondPageReview = () => {
   return (
@@ -13,19 +14,44 @@ const SecondPageReview = () => {
           </Button>
         </DrawerTrigger>
         <DrawerContent className='bg-black border-l-lime-500'>
-          <DrawerHeader className='text-center'>Menu</DrawerHeader>
+          <DrawerHeader className='text-center flex flex-row w-full items-center justify-between'>
+            Menu
+            <DrawerClose>
+              <X />
+            </DrawerClose>
+          </DrawerHeader>
           <Separator
             orientation='horizontal'
             className='bg-stone-300'
           />
           <DrawerDescription className='p-3 font-bold'>Drawer Description</DrawerDescription>
           <div className='w-11/12 mx-auto border text-stone-500 border-dashed border-stone-700 rounded-lg p-3 h-44 my-4'>
-            CONTENT HERE ...
+            <DrawerTitle> CONTENT HERE ...</DrawerTitle>
           </div>
           <Separator
             orientation='horizontal'
             className='bg-stone-300'
           />
+          <ul className='flex flex-col gap-3 p-3'>
+            <Link
+              href={"/"}
+              className='flex items-center-safe gap-3 hover:bg-lime-800 p-2 rounded-lg'>
+              <Home size={18} />
+              Home
+            </Link>
+            <Link
+              href={"/"}
+              className='flex items-center-safe gap-3 hover:bg-lime-800 p-2 rounded-lg'>
+              <Info size={18} />
+              Info
+            </Link>
+            <Link
+              href={"/"}
+              className='flex items-center-safe gap-3 hover:bg-lime-800 p-2 rounded-lg'>
+              <Code size={18} />
+              Developer Tools
+            </Link>
+          </ul>
           <DrawerFooter>
             <Button
               variant={"lime"}
