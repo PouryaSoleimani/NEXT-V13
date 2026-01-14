ca6dc99113542f75ae99496684cfc093daa99be2
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-14 21:07:29 +0330

    FRONT__REVIEW

--- app/review/second/page.tsx
+++ app/review/second/page.tsx
@@ -1,17 +1,43 @@
 'use client'
 import { Button } from "@/components/ui/button";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuSeparator,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
 import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
 import { Separator } from "@/components/ui/separator";
-import { Code, Home, Info, Menu, X } from "lucide-react";
+import {
+  ArrowLeft,
+  ArrowRight,
+  Code,
+  Code2Icon,
+  Copy,
+  Home,
+  Info,
+  Menu,
+  PlusCircle,
+  Save,
+  X,
+  EditIcon,
+  Trash2Icon,
+  MoreHorizontal,
+} from "lucide-react";
 import Link from "next/link";
 import toast from "react-hot-toast";
 
 const SecondPageReview = () => {
   return (
     <section className='section relative insert-0'>
+      {/* HEADER + DRAWER */}
       <div
         id='HEADER'
-        className='w-full absolute top-0 max-w-[1980px] px-1 mx-auto py-3'>
+        className='w-full absolute top-0 max-w-[92%] px-2 mx-auto py-3'>
         <Drawer direction='right'>
           <DrawerTrigger>
             <Button variant={"lime"}>
@@ -70,6 +96,64 @@ const SecondPageReview = () => {
           </DrawerContent>
         </Drawer>
       </div>
+
+      {/* CONTEXT MENU */}
+      <ContextMenu>
+        <ContextMenuTrigger className='border border-dashed border-stone-600 p-30 rounded-xl'>
+          Right Click Here
+        </ContextMenuTrigger>
+        <ContextMenuContent className='p-1 bg-white border-black border-4 text-stone-900'>
+          <ContextMenuItem className='hover:text-white'>
+            <ArrowLeft />
+            Back
+          </ContextMenuItem>
+          <ContextMenuItem className='hover:text-white'>
+            <ArrowRight />
+            Forward
+          </ContextMenuItem>
+          <ContextMenuItem className='hover:text-white'>
+            <Copy />
+            Copy
+          </ContextMenuItem>
+          <ContextMenuItem className='hover:text-white'>
+            <Code />
+            Developer Tools
+          </ContextMenuItem>
+          <ContextMenuSeparator className='bg-stone-800' />
+          <ContextMenuSub>
+            <ContextMenuSubTrigger className='gap-2 hover:bg-stone-800 hover:text-white'>
+              <MoreHorizontal />
+              More Tools
+            </ContextMenuSubTrigger>
+            <ContextMenuSubContent className='w-44 bg-white *:hover:text-white translate-x-4'>
+              <ContextMenuItem>
+                <Save />
+                Save Page...
+              </ContextMenuItem>
+              <ContextMenuItem>
+                <PlusCircle />
+                Create Shortcut...
+              </ContextMenuItem>
+              <ContextMenuItem>
+                <EditIcon />
+                Name Window...
+              </ContextMenuItem>
+              <ContextMenuSeparator className='bg-stone-800' />
+              <ContextMenuItem>
+                <Code2Icon />
+                Developer Tools
+              </ContextMenuItem>
+              <ContextMenuSeparator className='bg-stone-800' />
+              <ContextMenuItem
+                variant='destructive'
+                className=' hover:bg-red-900'>
+                <Trash2Icon />
+                Delete
+              </ContextMenuItem>
+            </ContextMenuSubContent>
+          </ContextMenuSub>
+        </ContextMenuContent>
+      </ContextMenu>
     </section>
   );
 }
