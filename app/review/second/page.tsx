17d235a9ba496cd916e5a7eaa12c8c8c6273d60a
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-14 20:52:52 +0330

    FRONT__REVIEW

--- app/review/second/page.tsx
+++ app/review/second/page.tsx
@@ -6,61 +6,65 @@ import Link from "next/link";
 
 const SecondPageReview = () => {
   return (
-    <section className='section'>
-      <Drawer direction='right'>
-        <DrawerTrigger>
-          <Button variant={"lime"}>
-            <Menu />
-          </Button>
-        </DrawerTrigger>
-        <DrawerContent className='bg-black border-l-lime-500'>
-          <DrawerHeader className='text-center flex flex-row w-full items-center justify-between'>
-            Menu
-            <DrawerClose>
-              <X />
-            </DrawerClose>
-          </DrawerHeader>
-          <Separator
-            orientation='horizontal'
-            className='bg-stone-300'
-          />
-          <DrawerDescription className='p-3 font-bold'>Drawer Description</DrawerDescription>
-          <div className='w-11/12 mx-auto border text-stone-500 border-dashed border-stone-700 rounded-lg p-3 h-44 my-4'>
-            <DrawerTitle> CONTENT HERE ...</DrawerTitle>
-          </div>
-          <Separator
-            orientation='horizontal'
-            className='bg-stone-300'
-          />
-          <ul className='flex flex-col gap-3 p-3'>
-            <Link
-              href={"/"}
-              className='flex items-center-safe gap-3 hover:bg-lime-800 p-2 rounded-lg'>
-              <Home size={18} />
-              Home
-            </Link>
-            <Link
-              href={"/"}
-              className='flex items-center-safe gap-3 hover:bg-lime-800 p-2 rounded-lg'>
-              <Info size={18} />
-              Info
-            </Link>
-            <Link
-              href={"/"}
-              className='flex items-center-safe gap-3 hover:bg-lime-800 p-2 rounded-lg'>
-              <Code size={18} />
-              Developer Tools
-            </Link>
-          </ul>
-          <DrawerFooter>
-            <Button
-              variant={"lime"}
-              className='w-full h-16'>
-              SUBMIT
+    <section className='section relative insert-0'>
+      <div
+        id='HEADER'
+        className='w-full absolute top-0 max-w-[1980px] px-1 mx-auto py-3'>
+        <Drawer direction='right'>
+          <DrawerTrigger>
+            <Button variant={"lime"}>
+              <Menu />
             </Button>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
+          </DrawerTrigger>
+          <DrawerContent className='bg-black border-l-lime-500'>
+            <DrawerHeader className='text-center flex flex-row w-full items-center justify-between'>
+              Menu
+              <DrawerClose asChild>
+                <X />
+              </DrawerClose>
+            </DrawerHeader>
+            <Separator
+              orientation='horizontal'
+              className='bg-stone-300'
+            />
+            <DrawerDescription className='p-3 font-bold'>Drawer Description</DrawerDescription>
+            <div className='w-11/12 mx-auto border text-stone-500 border-dashed border-stone-700 rounded-lg p-3 h-44 my-4'>
+              <DrawerTitle> CONTENT HERE ...</DrawerTitle>
+            </div>
+            <Separator
+              orientation='horizontal'
+              className='bg-stone-300'
+            />
+            <ul className='flex flex-col gap-3 p-3'>
+              <Link
+                href={"/"}
+                className='flex items-center-safe gap-3 hover:bg-lime-800 px-2 py-3 rounded-lg'>
+                <Home size={18} />
+                Home
+              </Link>
+              <Link
+                href={"/"}
+                className='flex items-center-safe gap-3 hover:bg-lime-800 px-2 py-3 rounded-lg'>
+                <Info size={18} />
+                Info
+              </Link>
+              <Link
+                href={"/"}
+                className='flex items-center-safe gap-3 hover:bg-lime-800 px-2 py-3 rounded-lg'>
+                <Code size={18} />
+                Developer Tools
+              </Link>
+            </ul>
+            <DrawerFooter>
+              <Button
+                variant={"lime"}
+                className='w-full h-16'>
+                SUBMIT
+              </Button>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      </div>
     </section>
   );
 }
