7a60967c99f9d80d0f9f0b4cb31cac43c74be874
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-16 22:36:12 +0330

    NEXT__SEO

--- app/products/layout.tsx
+++ app/products/layout.tsx
@@ -1,7 +1,11 @@
 import { Metadata } from "next";
 
 export default async function generateMetaData({params} : {params : {id : string }}) :Promise<Metadata>{
-  const product = await fetch(`https://example.api.com/${params.id}`) as any;
+  const product = (await fetch(`https://example.api.com/${params.id}`)) as any;
 
-return { title: product.title, description: product.description, openGraph: { images: [product.image] } };
+  return {
+    title: product.title,
+    description: product.description,
+    openGraph: { images: [product.image] },
+  };
 }
