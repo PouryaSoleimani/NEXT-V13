818a433aaccd9647c98648a15061afd23ca6b646
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-17 20:38:56 +0330

    [ADD] CSS RESET

--- app/styling/page.tsx
+++ app/styling/page.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const StylingPage = () => {
   return (
     <div className='w-full-width border center p-10 rounded-default border-stone-700  mx-auto'>
-      <div className="p-32 border rounded-default border-brand">HELLO</div>
+      <div className='p-32 border bg-brand rounded-default border-brand'>
+        HELLO
+      </div>
     </div>
   );
 };
