497cfead81058ce81eb56ee81a470973c62b3494
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-17 20:31:27 +0330

    [ADD] - CSS VARIABLES

--- app/products/layout.tsx
+++ app/products/layout.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from "next";
-
 export default async function generateMetaData({
   params,
 }: {
---
+++ app/styling/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StylingPage = () => {
+  return (
+    <div className='w-full-width border center p-10 rounded-default border-stone-700  mx-auto'>
+      <div className="p-32 border rounded-default border-brand">HELLO</div>
+    </div>
+  );
+};
+
+export default StylingPage;
