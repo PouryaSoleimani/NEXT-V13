d46a31a31fa862a3b20606d8854431626bdeb118
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-17 21:05:24 +0330

    [ADD] Custom Layout.tsx form styling page

--- app/styling/layout.tsx
+++ app/styling/layout.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from "next";
 import React from "react";
 
+//? METADATA
+export const metadata: Metadata = { title: "CUSTOM STYLING PAGE" };
+
+//^ LAYOUT
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
@@ -7,6 +12,9 @@ const layout = ({ children }: { children: React.ReactNode }) => {
         CUSTOM HEADER
       </div>
       {children}
+      <div className='bg-blue-500 text-black w-full py-3 text-center'>
+        CUSTOM FOOTER
+      </div>
     </>
   );
 };
--- app/styling/page.tsx
+++ app/styling/page.tsx
@@ -1,7 +1,6 @@
-
 const StylingPage = () => {
   return (
-    <div className='w-full-width border center p-10 rounded-default border-stone-700  mx-auto'>
+    <div className='w-full-width border center p-10 border-stone-700  mx-auto'>
       <div className='p-32 border bg-brand rounded-default border-brand'>
         HELLO
         <input
