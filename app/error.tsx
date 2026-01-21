5d0410be8b16ea58eff3bcd8044cf1bc4f05bcd4
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-21 16:10:00 +0330

    PROMISES

--- app/error.tsx
+++ app/error.tsx
@@ -2,6 +2,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { AlertTriangle, RefreshCw } from "lucide-react";
+import { ErrorIcon } from "react-hot-toast";
 
 export default function ErrorBoundary({
   error,
@@ -14,19 +15,22 @@ export default function ErrorBoundary({
 
   return (
     <div className='w-screen h-screen flex font-bold items-center justify-center'>
-      <div className='bg-zinc-800/300 flex flex-col  justify-center items-center gap-3 w-200 h-100 backdrop-blur-2xl rounded-lg border border-zinc-800 shadow-md shadow-black'>
-        <p className='text-red-500/70 bg-black p-3 rounded-lg flex items-center-safe gap-3'>
-          <AlertTriangle className='text-red-500/70 text-xl' />
+      <div className='flex flex-col  justify-center items-center gap-3 w-200 h-100 backdrop-blur-2xl rounded-lg border-zinc-800'>
+        <p className='text-red-500/70 text-3xl underline decoration-4 underline-offset-8 p-3 rounded-lg flex items-center-safe gap-3'>
+          <AlertTriangle className='text-red-500/70 text-xl size-10' />
           {error.name}
         </p>
-        <p className='text-stone-200 text-xl tracking-tighter'>
+        <p className='text-stone-200 text-2xl pb-3 border-b-2 border-stone-500 tracking-tighter'>
           {error.message}
         </p>
-        <details className='min-sm:text-white w-full  mx-auto text-ellipsis overflow-hidden'>
-          <summary>LOGS : </summary>
-          <pre dir="ltr">{error.stack}</pre>
-        </details>
+        <div className="center my-3 w-[90%] bg-black p-2.5 rounded-md">
+          <details className='min-sm:text-white w-full  mx-auto text-ellipsis overflow-hidden'>
+            <summary className="flex gap-3 font-bold font-sans"><ErrorIcon />ERROR DETAILS </summary>
+            <pre dir="ltr">{error.stack}</pre>
+          </details>
+        </div>
         <Button
+          variant={'blue'}
           onClick={() => reset()}
           className='w-fit my-6'>
           <RefreshCw />
--- app/promises/page.tsx
+++ app/promises/page.tsx
@@ -1,13 +1,19 @@
+import { CheckSquare2Icon } from "lucide-react"
 
 async function Page(props: PageProps<'/promises'>) {
   console.info('PROPS => ', props)
-  const _result = await promiseHandler(2)
+  const _result = await promiseHandler2('POURYA')
 
   if (!_result) {
     return <div className="section bg-rose-800/30 p-3 center">NOT CORRECT</div>
   }
   return (
-    <div className="section center bg-emerald-900/30">CORRECT</div>
+    <div className="section center">
+      <span className="flex bg-emerald-600 gap-3 p-3 rounded-lg border-4 border-emerald-600">
+        <CheckSquare2Icon />
+        CORRECT
+      </span>
+    </div>
   )
 }
 export default Page
@@ -20,6 +26,18 @@ async function promiseHandler(num: number) {
       }, 2000);
     } else {
       reject('NUM IS NOT CORRECT')
+    }
+  })
+}
+
+async function promiseHandler2(val: string) {
+  return new Promise((resolve, reject) => {
+    if (val.trim().toLowerCase() == 'pourya') {
+      setTimeout(() => {
+        resolve(val)
+      }, 3000);
+    } else {
+      reject('VAL IS NOT CORRECT')
     }
   })
 }
