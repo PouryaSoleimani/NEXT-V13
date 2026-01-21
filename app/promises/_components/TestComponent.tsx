bd6d214f13326a2781b714c1811710f56150c33b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-21 16:53:25 +0330

    SKELETON + SUSPENSE

---
+++ app/promises/_components/TestComponent.tsx
@@ -0,0 +1,30 @@
+import { CheckSquare2Icon } from 'lucide-react'
+
+const TestComponent = async () => {
+  const _result = await testPromiseHandler('reza')
+
+  if (!_result) {
+    <div>VAL IS NOT CORRECT</div>
+  }
+
+  return (
+    <span className="flex bg-emerald-600 gap-3 p-3 rounded-lg border-4 border-emerald-600">
+      <CheckSquare2Icon />
+      CORRECT
+    </span>
+  )
+}
+
+export default TestComponent
+
+async function testPromiseHandler(val: string) {
+  return new Promise((resolve, reject) => {
+    if (val.trim().toLowerCase() === 'reza') {
+      setTimeout(() => {
+        resolve(val)
+      }, 5000);
+    } else {
+      reject('TEST COMPONENT ERROR ...  ')
+    }
+  })
+}
--- app/promises/page.tsx
+++ app/promises/page.tsx
@@ -1,8 +1,13 @@
+import { Skeleton } from "@/components/ui/skeleton"
 import { CheckSquare2Icon } from "lucide-react"
+import { Suspense } from "react"
+import { isIfStatement } from "typescript"
+import TestComponent from "./_components/TestComponent"
+import SkeletonComponentV4 from "./_components/SkeletonComponentV4"
 
 async function Page(props: PageProps<'/promises'>) {
   console.info('PROPS => ', props)
-  const _result = await promiseHandler2('POURYA')
+  const _result = await promiseHandler4('mamad')
 
   if (!_result) {
     return <div className="section bg-rose-800/30 p-3 center">NOT CORRECT</div>
@@ -10,10 +15,9 @@ async function Page(props: PageProps<'/promises'>) {
 
   return (
     <div className="section center">
-      <span className="flex bg-emerald-600 gap-3 p-3 rounded-lg border-4 border-emerald-600">
-        <CheckSquare2Icon />
-        CORRECT
-      </span>
+      <Suspense fallback={<SkeletonComponentV4 />}>
+        <TestComponent />
+      </Suspense>
     </div>
   )
 }
@@ -53,4 +57,17 @@ async function promiseHandler3(bool: boolean) {
       reject('BOOL IS NOT CORRECT')
     }
   })
+}
+
+async function promiseHandler4(val: string) {
+  return new Promise((resolve, reject) => {
+    if (val.trim().toLowerCase() == 'mamad') {
+      setTimeout(() => {
+        resolve(val)
+      }, 1500);
+    } else {
+      reject('VAL IS NOT CORRECT AGAIN')
+    }
+  })
+
 }
