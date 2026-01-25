a14073489498180f3790b221a1cf15dd06579685
PouryaSoleimani <pouryasoleimani93@gmail.com>
2026-01-25 21:27:54 +0330

    API___ROUTES

--- app/employees/page.tsx
+++ app/employees/page.tsx
@@ -12,12 +12,25 @@ const Page = () => {
     return <div>IS LOADING</div>
   }
 
+  function toastHandler(name: string) {
+    toast.success(name.toUpperCase(), {
+      position: "top-center",
+      style: {
+        backgroundColor: 'black',
+        color: '#f5f5f5',
+        width: '300px',
+        borderBottom: '3px solid #f5f5f5'
+      }
+    })
+  }
+
+
   console.info('RESPONSE FROM API ROUTE => ', data)
 
   return (
     <div className='grid grid-cols-4 p-32 gap-3'>{data?.data.map((employee: { id: number, name: string, job: string }) => (
       <button
-        onClick={() => { toast.success(employee.name, { position: "top-center", style: { backgroundColor: 'black', color: '#f5f5f5', width: '300px', borderBottom: '3px solid #f5f5f5' } }) }}
+        onClick={() => { toastHandler(employee.name) }}
         key={employee.id}
         className='grid cursor-pointer hover:bg-primary hover:border-secondary hover:text-foreground text-stone-300 transition-all duration-300  place-items-center border-8 bg-stone-800 px-4 py-2 rounded-md border-stone-900'
       >
